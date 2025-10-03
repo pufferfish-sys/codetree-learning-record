6e9c8d48b21f682d4eeb18017f30e0c31972c4eb
pufferfish-sys <sonbal7762@gmail.com>
2025-10-03 20:05:02 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251003/중복되지 않는 정수 중 최대/max-of-unique-number.c
+++ 251003/중복되지 않는 정수 중 최대/max-of-unique-number.c
@@ -1,26 +1,21 @@
 #include <stdio.h>
 
 int main() {
-    // Please write your code here.
-    int n,k,arr[1000],max=-1;
-    scanf("%d ",&n);
-    for(int i=0;i<n;i++){
-        scanf("%d ",&arr[i]);
+    int n, arr[1000], count[1000] = {0,}, max = -1;
+    scanf("%d", &n);
+
+    for(int i = 0; i < n; i++){
+        scanf("%d", &arr[i]);
+        count[arr[i]]++;
     }
-    for(int i=0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-            if(arr[j]!=-1&&arr[i]==arr[j]){
-                arr[i]=-1;
-                arr[j]=-1;
-            }
-        }
-    }
-    for(int i=0;i<n;i++){
-        if(arr[i]>max){
+
+    for(int i = 0; i < n; i++){
+        if(count[arr[i]] == 1 && arr[i] > max){
             max = arr[i];
         }
     }
-    printf("%d",max);
+
+    printf("%d", max);
 
     return 0;
 }

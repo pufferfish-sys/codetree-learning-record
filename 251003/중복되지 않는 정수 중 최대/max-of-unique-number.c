c20aeb81a0fac541b3bcf2ed2146082db6bf9a16
pufferfish-sys <sonbal7762@gmail.com>
2025-10-03 20:00:46 +0900

    Title: 중복되지 않는 정수 중 최대, Time: 9ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251003/중복되지 않는 정수 중 최대/max-of-unique-number.c
+++ 251003/중복되지 않는 정수 중 최대/max-of-unique-number.c
@@ -9,7 +9,7 @@ int main() {
     }
     for(int i=0;i<n;i++){
         for(int j=i+1;j<n;j++){
-            if(arr[i]==arr[j]){
+            if(arr[j]!=-1&&arr[i]==arr[j]){
                 arr[i]=-1;
                 arr[j]=-1;
             }

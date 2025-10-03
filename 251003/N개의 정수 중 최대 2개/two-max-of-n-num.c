823c35342e57722ebc07210645c8419d0dafb726
pufferfish-sys <sonbal7762@gmail.com>
2025-10-03 19:40:28 +0900

    Title: N개의 정수 중 최대 2개, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251003/N개의 정수 중 최대 2개/two-max-of-n-num.c
+++ 251003/N개의 정수 중 최대 2개/two-max-of-n-num.c
@@ -2,19 +2,23 @@
 
 int main() {
     // Please write your code here.
-    int n,arr[100],k,max=0,max2=0;
+    int n,arr[100],max=0,max2=0,k;
     scanf("%d ",&n);
     for(int i=0;i<n;i++){
         scanf("%d ",&arr[i]);
-        k=i;
     }
     for(int i=0;i<n;i++){
         if(arr[i]>=max){
             max=arr[i];
+            k=i;
         }
     }
+
     for(int i=0;i<n;i++){
-        if(arr[i]>=max2&&arr[i]<max){
+        if(i==k){
+            continue;
+        }
+        if(arr[i]>=max2){
             max2=arr[i];
         }
     }

c7b77241a11a46961d88ee8f2718a63131c65550
pufferfish-sys <sonbal7762@gmail.com>
2025-10-03 16:31:50 +0900

    Title: 1-9 개수 세기, Time: 16ms, Memory: 0MB, Status: Runtime Error - Codetree

---
+++ 251003/1-9 개수 세기/count-one-to-nine.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,arr[10],count[10]={0,};
+    scanf("%d",&n);
+    for(int i=0;i<n;i++){
+        scanf("%d ",&arr[i]);
+    }
+    for(int i=0;i<n;i++){
+        count[arr[i]]++;
+    }
+    for(int i=1;i<=9;i++){
+        printf("%d\n",count[i]);
+    }
+    return 0;
+}

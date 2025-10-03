ce8ef3cfa255c6f084db3326dea9a30ec68f2bc9
pufferfish-sys <sonbal7762@gmail.com>
2025-10-03 18:20:17 +0900

    Title: 연속부분수열일까, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251003/연속부분수열일까/contiguous-array-or-not.c
+++ 251003/연속부분수열일까/contiguous-array-or-not.c
@@ -1,37 +1,24 @@
 #include <stdio.h>
 
 int main() {
-    int n1, n2, arr1[100], arr2[100], cnt = 0;
+    int n1, n2, arr1[100], arr2[100], cnt = 0, k = 0;
     scanf("%d %d", &n1, &n2);
+    for(int i = 0; i < n1; i++) scanf("%d", &arr1[i]);
+    for(int i = 0; i < n2; i++) scanf("%d", &arr2[i]);
 
     for(int i = 0; i < n1; i++){
-        scanf("%d", &arr1[i]);
-    }
-    for(int i = 0; i < n2; i++){
-        scanf("%d", &arr2[i]);
-    }
-
-    int found = 0;
-    for(int i = 0; i <= n1 - n2; i++){
-        cnt = 0;
-        for(int j = 0; j < n2; j++){
-            if(arr1[i + j] == arr2[j]){
-                cnt++;
-            } else {
-                break;
-            }
-        }
-        if(cnt == n2){
-            found = 1;
-            break;
+        if(arr1[i] == arr2[k]){
+            cnt++;
+            k++;
+            if(cnt == n2) break;
+        } else {
+            cnt = 0;
+            k = 0;
         }
     }
 
-    if(found){
-        printf("Yes\n");
-    } else {
-        printf("No\n");
-    }
+    if(cnt == n2) printf("Yes");
+    else printf("No");
 
     return 0;
 }

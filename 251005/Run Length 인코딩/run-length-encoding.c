efd81bf173af37bd50de2d28a18ad9151a7779a2
pufferfish-sys <sonbal7762@gmail.com>
2025-10-05 02:22:57 +0900

    Title: Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251005/Run Length 인코딩/run-length-encoding.c
+++ 251005/Run Length 인코딩/run-length-encoding.c
@@ -1,43 +1,35 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    // Please write your code here.
     char a[1000];
-    scanf("%s",a);
+    scanf("%s", a);
     int len = strlen(a);
-    int i = 0,c=0,length=0;
-    while(i<len){
-        int sum=0,cnt=0,b;
-        while(a[i]==a[c]){
+    int i = 0, c = 0, length = 0;
+    
+    while (i < len) {
+        int cnt = 0;
+        while (c < len && a[i] == a[c]) {
+            c++;
+            cnt++;}
+        int b;
+        if (cnt < 10) b = 1;
+        else if (cnt < 100) b = 2;
+        else if (cnt < 1000) b = 3;
+        else b = 4;
+        length += 1 + b;
+        i = c;
+    }
+    printf("%d\n", length);
+    i = 0; c = 0;
+    while (i < len) {
+        int cnt = 0;
+        while (c < len && a[i] == a[c]) {
             c++;
             cnt++;
         }
-        if(cnt<10){b=1;}
-        else if(cnt>=10&&cnt<100){
-            b=2;
-        }
-        else if(cnt>=100&&cnt<1000){
-            b=3;
-        }
-        else{
-            b=4;
-        }
-        length+=1+b;
-        i=c;
-    }
-    c=0;i=0;
-    printf("%d\n",length);
-
-    while(i<len){
-        int sum=0;
-        while(a[i]==a[c]){
-            sum++;
-            c++;
-        }
-        printf("%c%d",a[i],sum);
+        printf("%c%d", a[i], cnt);
         i = c;
     }
-
-
     return 0;
 }

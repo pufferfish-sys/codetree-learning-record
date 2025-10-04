4810e1bd19d358c71ed0ccd2e1d579ace8c250bc
pufferfish-sys <sonbal7762@gmail.com>
2025-10-05 01:24:38 +0900

    Title: 문자열 길이의 합, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251005/문자열 길이의 합/sum-length-of-string.c
+++ 251005/문자열 길이의 합/sum-length-of-string.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include <string.h>
+
 int main() {
-    // Please write your code here..
     char list[10][101];
-    int n,cnt=0,sum=0;
-    scanf("%d",&n);
-    for(int i=0;i<n;i++){
-        scanf("%s",list[i]);
+    int n, cnt = 0, sum = 0;
+
+    scanf("%d", &n);
+    // 문자열 입력
+    for(int i = 0; i < n; i++) {
+        scanf("%s", list[i]);
     }
-    for(int i=0;i<n;i++){
-        for(int j=0;j<strlen(list[i]);j++){
-            if(list[i][j]=='a')cnt++;
+
+    for(int i = 0; i < n; i++) {
+        int len = strlen(list[i]);
+        sum += len;
+        for(int j = 0; j < len; j++) {
+            if(list[i][j] == 'a') cnt++;
         }
     }
-    for(int i=0;i<n;i++){
-        sum+=strlen(list[i]);
-    }
-    printf("%d %d",sum,cnt);
+
+    printf("%d %d", sum, cnt);
+
     return 0;
 }

08e47940d3e702c43830ce9026d017ac833f3658
pufferfish-sys <sonbal7762@gmail.com>
2025-10-04 18:28:32 +0900

    Title: 격자에 점 그리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251004/격자에 점 그리기/draw-points-on-grid.c
+++ 251004/격자에 점 그리기/draw-points-on-grid.c
@@ -3,13 +3,8 @@
 int main() {
    
     int n,m,r,c;
+    int arr[10][10]={0,};
     scanf("%d %d",&n,&m);
-    int arr[n][n];
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            arr[i][j]=0;
-        }
-    }
     for(int i=1;i<=m;i++){
         scanf("%d %d",&r,&c);
         arr[r-1][c-1]=i;

c6acc12f7f1161be5e31858735b5f20e496a63cc
pufferfish-sys <sonbal7762@gmail.com>
2025-10-05 02:31:09 +0900

    Title: Run Length 인코딩, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251005/Run Length 인코딩/run-length-encoding.c
+++ 251005/Run Length 인코딩/run-length-encoding.c
@@ -3,33 +3,21 @@
 
 int main() {
     char a[1000];
+    char b[1000];
     scanf("%s", a);
     int len = strlen(a);
-    int i = 0, c = 0, length = 0;
-    
-    while (i < len) {
-        int cnt = 0;
-        while (c < len && a[i] == a[c]) {
-            c++;
-            cnt++;}
-        int b;
-        if (cnt < 10) b = 1;
-        else if (cnt < 100) b = 2;
-        else if (cnt < 1000) b = 3;
-        else b = 4;
-        length += 1 + b;
-        i = c;
-    }
-    printf("%d\n", length);
-    i = 0; c = 0;
+    int i = 0, c = 0;
     while (i < len) {
         int cnt = 0;
         while (c < len && a[i] == a[c]) {
             c++;
             cnt++;
         }
-        printf("%c%d", a[i], cnt);
+        char temp[5];
+        sprintf(temp, "%c%d", a[i], cnt);
+        strcat(b, temp); 
         i = c;
     }
+    printf("%d\n%s",strlen(b),b);  // 압축 결과 출력
     return 0;
 }

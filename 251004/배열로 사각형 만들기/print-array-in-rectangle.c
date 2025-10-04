8e7d75f457507d1f2b514cbd084c71d622a1ec17
pufferfish-sys <sonbal7762@gmail.com>
2025-10-04 17:37:39 +0900

    Title: 배열로 사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251004/배열로 사각형 만들기/print-array-in-rectangle.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int arr[5][5];
+    for(int i=0;i<5;i++){
+        arr[0][i]=1;
+        arr[i][0]=1;
+    }
+    for(int i=1;i<5;i++){
+        for(int j=1;j<5;j++){
+            arr[i][j]=arr[i][j-1]+arr[i-1][j];
+        }
+    }
+    for(int i=0;i<5;i++){
+        for(int j=0;j<5;j++){
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+
+
+
+    return 0;
+}

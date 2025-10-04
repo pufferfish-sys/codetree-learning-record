2ac80fb5a7fd335c7e2dc876001c66d41e3bb89b
pufferfish-sys <sonbal7762@gmail.com>
2025-10-04 17:24:52 +0900

    Title: 격자 반대로 채우기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251004/격자 반대로 채우기/grid-anti-fill.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int n,cnt=1;
+    scanf("%d",&n);
+    int arr[n][n];
+    for(int j=n-1;j>=0;j--){
+        if(n%2==0){
+        if(j%2!=0){
+        for(int i=n-1;i>=0;i--){
+            arr[i][j]=cnt++;
+        }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                arr[i][j]=cnt++;
+            }
+        }}
+        else{
+            if(j%2==0){
+        for(int i=n-1;i>=0;i--){
+            arr[i][j]=cnt++;
+        }
+        }
+        else{
+            for(int i=0;i<n;i++){
+                arr[i][j]=cnt++;
+            }
+        }
+
+        }
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0;j<n;j++){
+            printf("%d ",arr[i][j]);
+        }printf("\n");
+    }
+    return 0;
+}

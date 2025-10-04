e5229d5065a0ae6ca9d2b75c2a6657e1d5495278
pufferfish-sys <sonbal7762@gmail.com>
2025-10-05 00:51:08 +0900

    Title: 공백을 기준으로 출력, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251005/공백을 기준으로 출력/output-based-on-space.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    // Please write your code here.
+    char str1[100],str2[100];
+    fgets(str1,100,stdin);
+    fgets(str2,100,stdin);
+    for(int i=0;i<strlen(str1)-1;i++){
+        if(str1[i]!=' '){
+            printf("%c",str1[i]);
+        }
+    }
+    for(int i=0;i<strlen(str2)-1;i++){
+        if(str2[i]!=' '){
+            printf("%c",str2[i]);
+        }
+    }
+    return 0;
+}

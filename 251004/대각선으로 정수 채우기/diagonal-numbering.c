e6b85994c9ab76d0feb8dc40530c7cfc35220e62
pufferfish-sys <sonbal7762@gmail.com>
2025-10-04 18:52:24 +0900

    Title: 대각선으로 정수 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251004/대각선으로 정수 채우기/diagonal-numbering.c
+++ 251004/대각선으로 정수 채우기/diagonal-numbering.c
@@ -1,21 +1,18 @@
 #include <stdio.h>
 
 int main() {
+    // Please write your code here.
     int n, m;
     scanf("%d %d", &n, &m);
     int arr[n][m];
-    int cnt = 1;
-    for (int i = 0; i < n + m - 1; i++) {
-        int row, col;
-        if(i<m){row=0; col=i;}
-        else{row=i-m+1; col=m-1;}
-        while(row<n&&col>=0){
-            arr[row][col]=cnt++;
-            row++;col--;
-        }
-    }
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) {
+    int num = 1;
+    for (int k = 0; k < n + m - 1; k++){
+        for (int i = 0; i < n; i++){
+            for (int j = 0; j < m; j++){
+                if (i + j == k)arr[i][j] = num++;}}}
+                
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < m; j++){
             printf("%d ", arr[i][j]);
         }
         printf("\n");

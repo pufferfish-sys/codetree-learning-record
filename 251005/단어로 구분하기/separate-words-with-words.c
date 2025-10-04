bd423b179737b064baa86364528a47b06938cee8
pufferfish-sys <sonbal7762@gmail.com>
2025-10-05 01:29:04 +0900

    Title: 단어로 구분하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251005/단어로 구분하기/separate-words-with-words.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char list[10][201];
+    for(int i=0;i<10;i++){
+        scanf("%s",list[i]);
+    }
+    for(int i=0;i<10;i++){
+        printf("%s\n",list[i]);
+    }
+    return 0;
+}

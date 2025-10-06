186cc772c83779011d2a497899f3147155a82aef
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 04:40:45 +0900

    Title: 반복 출력하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/반복 출력하기/repeated-output.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void star(int n){
+    for(int i=0;i<n;i++){
+    printf("12345^&*()_\n");}
+}
+
+int main() {
+    int row_num;
+    scanf("%d", &row_num);
+    star(row_num);
+    // Please write your code here.
+    return 0;
+}

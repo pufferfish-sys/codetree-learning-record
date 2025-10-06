4a1eab43f85305060dbee189421b65d4a55814bc
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 04:38:17 +0900

    Title: 별 찍는 것을 5번 반복하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/별 찍는 것을 5번 반복하기/repeat-shooting-the-stars-five-times.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+void star(){
+    for(int i=0;i<10;i++) printf("*");
+    printf("\n");
+}
+int main() {
+    // Please write your code here.
+    for(int i=0;i<5;i++) star();
+    return 0;
+}

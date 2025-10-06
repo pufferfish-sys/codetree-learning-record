2667f92325a01de088cf7aff5cfdd7cb58da867b
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 01:07:33 +0900

    Title: 그 다음 알파벳, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/그 다음 알파벳/next-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c",&a);
+    int n = (int)a;
+    if(a=='z'){printf("a");}
+    else{printf("%c",n+1);}
+    return 0;
+}

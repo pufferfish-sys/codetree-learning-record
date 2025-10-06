e83c2d2ae54daefcc38313995d8de7b61a0010bb
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 01:08:25 +0900

    Title: 그 전 알파벳, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/그 전 알파벳/before-the-alphabet.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    char a;
+    scanf("%c",&a);
+    int n = (int)a;
+    if(a=='a'){printf("z");}
+    else{printf("%c",n-1);}
+    return 0;
+}

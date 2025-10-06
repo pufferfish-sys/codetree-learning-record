d9099d2c3a71ea0ed4e70e5615026281ca750d05
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 00:59:50 +0900

    Title: 아스키코드표 맞추기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/아스키코드표 맞추기/chart-of-ASCII.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Please write your code here.
+    int a;
+    for(int i=0;i<5;i++){
+        scanf("%d ",&a);
+        printf("%c ",a);
+    }
+    return 0;
+}

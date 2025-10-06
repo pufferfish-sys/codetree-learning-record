3641fd22da26cdf5183088789ac5fc554228281a
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 04:35:22 +0900

    Title: 미는 횟수, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251007/미는 횟수/number-of-pushes.c
+++ 251007/미는 횟수/number-of-pushes.c
@@ -1,26 +1,27 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 int main() {
-    char arr[100], arr2[100], temp;
-    int cnt = 0;
-
-    scanf("%s", arr);
-    scanf("%s", arr2);
+    int n,cnt=0;
+    char arr[100],arr2[100],temp;
+    scanf("%s",arr);
+    scanf("%s",arr2);
     int len = strlen(arr);
 
-    for (int j = 0; j < len; j++) {
-        if (strcmp(arr, arr2) == 0) {
-            printf("%d", cnt);
-            return 0;
-        }
-        temp = arr[len - 1];
+    for(int j=0;j<len;j++){
+             temp = arr[len - 1];
         for (int i = len - 1; i > 0; i--) {
             arr[i] = arr[i - 1];
         }
         arr[0] = temp;
-        cnt++;
+    cnt++;
+    if(strcmp(arr,arr2)==0)break;
+
     }
-    printf("-1");
+    if(cnt==len){printf("-1");}
+    else printf("%d", cnt);
     return 0;
 }
+

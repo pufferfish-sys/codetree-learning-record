00aaba37983bf8420d3b9fb549597ef61042ce31
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 03:42:11 +0900

    Title: 정수만 추출하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251007/정수만 추출하기/extract-only-integers.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main() {
+    char a[8], b[8];
+    scanf("%s", a);
+    scanf("%s", b);
+    for (int i = 0; i < strlen(a); i++) {
+        if (!isdigit(a[i])) {
+            a[i] = '\0';
+            break;
+        }
+    }
+    for (int i = 0; i < strlen(b); i++) {
+        if (!isdigit(b[i])) {
+            b[i] = '\0';
+            break;
+        }
+    }
+    printf("%d", atoi(a) + atoi(b));
+    return 0;
+}

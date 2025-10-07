f2b19bc5f41cef892f1fab9088ccf6c63e886cef
pufferfish-sys <sonbal7762@gmail.com>
2025-10-08 01:15:12 +0900

    Title: 정수의 최솟값, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251008/정수의 최솟값/minimum-value-of-an-integer.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+void minf(int A,int B,int C){
+    int min;
+    if(A>C&&B>C){
+        min = C;}
+    else if(A>B&&C>B){
+        min =B;
+    }
+    else{
+        min =A;
+    }
+printf("%d",min);
+}
+int main() {
+    int a, b, c;
+    scanf("%d %d %d", &a, &b, &c);
+    minf(a,b,c);
+    // Please write your code here.
+    
+    return 0;
+}

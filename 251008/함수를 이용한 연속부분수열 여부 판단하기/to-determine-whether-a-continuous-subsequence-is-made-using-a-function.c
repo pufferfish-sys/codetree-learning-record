5930b3585665f0579acfa73a3ec482041ff6636f
pufferfish-sys <sonbal7762@gmail.com>
2025-10-08 02:55:17 +0900

    Title: 함수를 이용한 연속부분수열 여부 판단하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251008/함수를 이용한 연속부분수열 여부 판단하기/to-determine-whether-a-continuous-subsequence-is-made-using-a-function.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int n1, n2;
+int a[100], b[100];
+
+void aff(){
+    int n = 0;
+    for(int i = 0; i < n1; i++){
+        if(a[i] == b[n]){
+            n++;
+        } else {
+            n = 0;
+        }
+        if(n == n2){
+            printf("Yes\n");
+            return;
+        }
+    }
+    printf("No\n");
+}
+
+int main() {
+    scanf("%d %d", &n1, &n2);
+    for(int i = 0; i < n1; i++)
+        scanf("%d", &a[i]);
+    for(int j = 0; j < n2; j++)
+        scanf("%d", &b[j]);
+    aff();
+    return 0;
+}

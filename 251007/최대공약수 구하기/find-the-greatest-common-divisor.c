b26b1d04889058b59e27436fe73c73ebdb3e3c77
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 16:37:51 +0900

    Title: 최대공약수 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251007/최대공약수 구하기/find-the-greatest-common-divisor.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+void star(int n,int m){
+    int a;
+    for(int i=1;i<=n;i++){
+        if(n%i==0&&m%i==0){
+        a=i;
+   }}
+   printf("%d",a);
+}
+int main() {
+    int N, M;
+    scanf("%d %d", &N, &M);
+    star(N,M);
+    // Please write your code here.
+    
+    return 0;
+}

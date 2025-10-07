aed4a4c8f1505b92c6684b0c99a6af0f48de44d9
pufferfish-sys <sonbal7762@gmail.com>
2025-10-07 16:46:18 +0900

    Title: 최소공배수 구하기, Time: 738ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251007/최소공배수 구하기/find-the-least-common-multiple.c
+++ 251007/최소공배수 구하기/find-the-least-common-multiple.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-void star(int n,int m){
-    int a=m;
-    for(int i=n;i<=10000;i+=n){
-        if(i==a){printf("%d",i);break;}
-        if(i>a){a+=m;}
-   }
+void star(int n, int m) {
+    int a = m;
+    for (int i = n; ; i += n) {
+        if (i == a) {
+            printf("%d", i);
+            break;
+        }
+        if (i > a) {
+            a += m;
+        }
+    }
 }
+
 int main() {
     int N, M;
     scanf("%d %d", &N, &M);
-    star(N,M);
-    // Please write your code here.
-    
+    star(N, M);
     return 0;
 }

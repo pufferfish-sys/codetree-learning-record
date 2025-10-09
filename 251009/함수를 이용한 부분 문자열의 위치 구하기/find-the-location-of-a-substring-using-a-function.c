e85fc79343bfd811546c734d72534f1f645b7c34
pufferfish-sys <sonbal7762@gmail.com>
2025-10-09 15:47:24 +0900

    Title: 함수를 이용한 부분 문자열의 위치 구하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251009/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.c
+++ 251009/함수를 이용한 부분 문자열의 위치 구하기/find-the-location-of-a-substring-using-a-function.c
@@ -2,26 +2,25 @@
 #include <string.h>
 char text[1001];
 char pattern[1001];
-void scan(char *a,char *b){
+int scan(char *a,char *b){
     int len1=strlen(a),len2=strlen(b),cnt=0;
     for(int i=0;i<len1;i++){
         for(int j=0;j<len2;j++){
             if(a[i+j]==b[j]){
                 cnt++;
-                if(cnt==len2){printf("%d",i); return;}           
+                if(cnt==len2){return i;}           
             }
             else{cnt=0;break;}
         }
     }
-    printf("-1");
-    return;
+    return -1;
 }
 
 
 int main() {
     scanf("%s", text);
     scanf("%s", pattern);
-    scan(text,pattern);
-    
+    int n = scan(text,pattern);
+    printf("%d",n);
     return 0;
 }

36a7bd80b1ccbb10c9076da2218be8f3533e8d57
pufferfish-sys <sonbal7762@gmail.com>
2025-10-11 02:02:40 +0900

    Title: 재귀함수를 이용한 최소공배수, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251011/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.c
+++ 251011/재귀함수를 이용한 최소공배수/least-common-multiple-using-recursive-function.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
-int gcd(int a, int b) {       // 두 수의 최대공약수
-    if (b == 0) return a;
-    return gcd(b, a % b);
+int n;
+int arr[11];
+
+int lcm(int a,int b){
+    int gcd =1;
+    int min = (a < b) ? a : b;
+    for(int i=1;i<=min;i++){
+        if(a%i==0 && b%i==0)
+        gcd = i;
+    }
+    return a*b/gcd;
+}
+int aff(int n){
+    if(n==1) return arr[1];
+    return lcm(aff(n-1),arr[n]);
 }
 
-int lcm(int arr[], int n) {   // n개의 수 LCM
-    if (n == 1) return arr[0];  // base case: 한 개 수면 자기 자신
-    int prev_lcm = lcm(arr, n - 1);   // 앞 n-1개 수의 LCM
-    return prev_lcm * arr[n - 1] / gcd(prev_lcm, arr[n - 1]);
-}
 
 int main() {
-    int n, arr[20];
     scanf("%d", &n);
-    for (int i = 0; i < n; i++)
+    for (int i = 1; i <= n; i++)
         scanf("%d", &arr[i]);
-
-    printf("%d\n", lcm(arr, n));
+    printf("%d",aff(n));
+    
     return 0;
 }

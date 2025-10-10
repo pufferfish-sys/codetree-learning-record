4929b90e2b28bfa0015a988fa2963329839c7b79
pufferfish-sys <sonbal7762@gmail.com>
2025-10-11 00:50:42 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 226ms, Memory: 80MB, Status: Memory Limit Exceed - Codetree

---
+++ 251011/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int arr[100];
+
+int maxf(int n){
+    if (n == 1) return arr[0];
+    int max = maxf(n - 1);
+    if (arr[n - 1] > max)
+        return arr[n - 1];
+    else
+        return max;
+}
+
+
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    printf("%d",maxf(n-1));
+    return 0;
+}

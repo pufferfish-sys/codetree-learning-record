e487801e6be58370da9b1c3ba847a489c1f4c7ff
pufferfish-sys <sonbal7762@gmail.com>
2025-10-11 00:37:51 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251011/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
+++ 251011/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.c
@@ -2,22 +2,14 @@
 
 int n1(int n){
     if(n==2) return n;
-    return n+n1(n-2);
-
-}
-
-int n2(int n){
     if(n==1) return n;
-    return n+n2(n-2);
+    return n+n1(n-2);
 }
-
-
 
 int main() {
     int n;
     scanf("%d", &n);
-    if(n%2==0) printf("%d",n1(n));
-    else printf("%d",n2(n));
+    printf("%d",n1(n));
     
     return 0;
 }

d2dcd3b328981918afcf44b3b92d3b2499089914
pufferfish-sys <sonbal7762@gmail.com>
2025-10-12 22:10:36 +0900

    Title: 중앙값 계산 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251012/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[1000]; // 최대 N 크기, 문제에서 N 범위 확인 필요
+
+    for (int i = 0; i < n; i++) {
+        int x;
+        cin >> x;
+        arr[i] = x;
+
+        if ((i + 1) % 2 == 1) { // 홀수 번째 입력
+            // 현재까지 입력된 배열 정렬
+            sort(arr, arr + i + 1);
+            // 중앙값 출력
+            cout << arr[i / 2] << " ";
+        }
+    }
+
+    return 0;
+}

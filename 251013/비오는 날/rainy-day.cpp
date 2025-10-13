c525ca75afc6bf1df747f8cc45580bf67ad4b2f7
pufferfish-sys <sonbal7762@gmail.com>
2025-10-13 19:28:06 +0900

    Title: 비오는 날, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251013/비오는 날/rainy-day.cpp
+++ 251013/비오는 날/rainy-day.cpp
@@ -1,54 +1,39 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-#define MAX_N 100
-
-class WeatherInfo {
+// Forecast 정보를 나타내는 클래스 선언
+class Forecast {
     public:
-        string date;
-        string day;
-        string weather;
-
-        WeatherInfo(string date, string day, string weather) {
+        string date, day, weather;
+        Forecast(string date, string day, string weather) {
             this->date = date;
             this->day = day;
             this->weather = weather;
         }
-
-        WeatherInfo() {}
 };
 
-int main() {
+Forecast ans = Forecast("9999-99-99", "", "");
+
+int main(){
+    // 변수 선언 및 입력
     int n;
     cin >> n;
 
-    int rain[MAX_N];    //비가 오는날의 index을 저장하는 배열
-    WeatherInfo wi[MAX_N];
-
-    for(int i=0; i<n; i++) {
+    for(int i = 1; i <= n; i++) {
         string date, day, weather;
         cin >> date >> day >> weather;
-
-        wi[i] = WeatherInfo(date, day, weather);
+        // Forecast 객체를 만들어 줍니다.
+        Forecast f = Forecast(date, day, weather);
+        if(weather == "Rain")
+            // 비가 오는 경우 가장 최근인지 확인하고,
+            // 가장 최근일 경우 정답을 업데이트합니다.
+            if(ans.date >= f.date)
+                ans = f;
     }
 
-    int idx = 0;    //비가 오는날이 몇 일인지를 구하기 위한 변수
-    for(int i=0; i<n; i++) {
-        if(wi[i].weather == "Rain")
-            rain[idx++] = i;    //rain[n] : n번째로 비오는 날의 인덱스
-    }
-
-    string min_date = wi[rain[0]].date;  //가장 빨리 비가 오는 날을 구하기 위해 설정한 최소값을 저장하는 변수
-    int min_idx = 0;        //최소값일 때의 index을 저장하기 위한 변수
-    for(int i=1; i<idx; i++)
-        if(wi[rain[i]].date < min_date) {
-            min_date = wi[rain[i]].date;
-            min_idx = i;
-        }
-
-    cout << wi[rain[min_idx]].date << " " << wi[rain[min_idx]].day << " " << wi[rain[min_idx]].weather;
-
-
+    // 결과를 출력합니다.
+    cout << ans.date << " " << ans.day << " " << ans.weather;
     return 0;
 }

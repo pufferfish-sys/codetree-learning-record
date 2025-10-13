2864a4375685a2a0ef1697a0ceebaa7447cbc20e
pufferfish-sys <sonbal7762@gmail.com>
2025-10-14 01:20:05 +0900

    Title: DateTime to DateTime, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251014/DateTime to DateTime/datetime-to-datetime.cpp
+++ 251014/DateTime to DateTime/datetime-to-datetime.cpp
@@ -6,9 +6,13 @@ int a, b, c;
 
 int main() {
     int day=11; int hour =11; int min =11;
-    int min2=0;
+    int min2=0; int flag =1;
     cin >> a >> b >> c;
-    while(true){
+    if(day>a){flag =0;min2 =-1;}
+    if(day==a&&hour>b){flag =0;min2 =-1;}
+    if(day==a&&hour==b&&min>c){flag =0;min2 =-1;}
+    while(flag){
+        
         if(day==a&&hour==b&&min==c) break;
         min++;
         min2++;

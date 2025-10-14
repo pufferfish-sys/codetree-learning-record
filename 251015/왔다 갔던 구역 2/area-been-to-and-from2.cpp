381f7900475b40978ca8b358a49e8bb1cd5f5b04
pufferfish-sys <sonbal7762@gmail.com>
2025-10-15 01:51:32 +0900

    Title: 왔다 갔던 구역 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251015/왔다 갔던 구역 2/area-been-to-and-from2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int x[100];
+char dir[100];
+int block[200]={0,};
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> x[i] >> dir[i];
+    }
+    int spot =100;
+    for(int i=0;i<n;i++){
+        if(dir[i]=='R'){
+        for(int j=spot;j<spot+x[i];j++){
+            block[j]++;
+        }spot+=x[i];
+        }
+        if(dir[i]=='L'){
+            for(int j=spot;j>spot-x[i];j--){
+                block[j]++;
+            }
+            spot-=x[i];
+        }
+
+    }
+    int cnt =0;
+    for(int i=0;i<200;i++){
+        if(block[i]>=2) cnt++;
+    }
+    cout << cnt;
+
+    return 0;
+}

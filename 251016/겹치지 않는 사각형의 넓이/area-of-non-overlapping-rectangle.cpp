2916fa85a31c93dc612602a8446966365abfe1f6
pufferfish-sys <sonbal7762@gmail.com>
2025-10-16 01:09:58 +0900

    Title: 겹치지 않는 사각형의 넓이, Time: 23ms, Memory: 16MB, Status: Passed - Codetree

--- 251016/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
+++ 251016/겹치지 않는 사각형의 넓이/area-of-non-overlapping-rectangle.cpp
@@ -10,22 +10,17 @@ int main() {
     cin >> x1[0] >> y1[0] >> x2[0] >> y2[0];
     cin >> x1[1] >> y1[1] >> x2[1] >> y2[1];
     cin >> x1[2] >> y1[2] >> x2[2] >> y2[2];
-    for(int i=0;i<2;i++){
+    for(int i=0;i<3;i++){
         for(int j=y1[i]+1000;j<y2[i]+1000;j++){
             for(int k=x1[i]+1000;k<x2[i]+1000;k++){
-                square[j][k]=1;
+                square[j][k]= i+1;
             }
         }
     }
-    for(int j=y1[2]+1000;j<y2[2]+1000;j++){
-            for(int k=x1[2]+1000;k<x2[2]+1000;k++){
-                square[j][k]=0;
-            }
-        }
         int cnt =0;
     for(int i=0;i<2000;i++){
         for(int j=0;j<2000;j++)
-        if(square[i][j]==1) cnt++;
+        if(square[i][j]==2||square[i][j]==1) cnt++;
     }
     cout << cnt;
 

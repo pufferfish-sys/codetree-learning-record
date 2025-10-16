ebdd358cb58b52c045a8f2285a3e31e8adc17c04
pufferfish-sys <sonbal7762@gmail.com>
2025-10-16 14:40:31 +0900

    Title: 벌금은 누구에게, Time: 10ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251016/벌금은 누구에게/who-will-pay.cpp
+++ 251016/벌금은 누구에게/who-will-pay.cpp
@@ -1,20 +1,20 @@
 #include <iostream>
-
 using namespace std;
 
 int N, M, K;
 int student[10000];
-int arr[10000]={0,};
+
 int main() {
     cin >> N >> M >> K;
-    for (int i = 0; i < M; i++) {
-        cin >> student[i];
-        arr[student[i]]++;
-    }
-    for(int i=1;i<=N;i++){
-        if(arr[i]>=K) {cout<<i; return 0;}
-    }
-    cout << -1;
+    for (int i = 0; i < M; i++) cin >> student[i];
 
+    for (int i = 0; i < N; i++) {
+        if (student[i] >= K) {
+            cout << i + 1;
+            return 0;
+        }
+    }
+
+    cout << -1;
     return 0;
 }

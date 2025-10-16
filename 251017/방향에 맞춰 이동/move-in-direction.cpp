d32cc545284a26a2c03558526d223f23c55112cb
pufferfish-sys <sonbal7762@gmail.com>
2025-10-17 01:58:59 +0900

    Title: 방향에 맞춰 이동, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251017/방향에 맞춰 이동/move-in-direction.cpp
+++ 251017/방향에 맞춰 이동/move-in-direction.cpp
@@ -5,26 +5,25 @@ using namespace std;
 int n;
 char dir[100];
 int dist[100];
-int dx[100]={0,},dy[100]={0,};
+int dx[4]={1,-1,0,0},dy[4]={0,0,1,-1};
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> dir[i] >> dist[i];
     }
-    for(int i=0;i<n;i++){
-        if(dir[i]=='N') dy[i]+=dist[i];
-        else if(dir[i]=='S') dy[i]-=dist[i];
-        else if(dir[i]=='W') dx[i]-=dist[i];
-        else if(dir[i]=='E') dx[i]+=dist[i];
-    }
     int x=0,y=0;
     for(int i=0;i<n;i++){
-        x+=dx[i];
-        y+=dy[i];
+        int dir2;
+        if(dir[i]=='N') dir2 =2;
+        else if(dir[i]=='S') dir2=3;
+        else if(dir[i]=='W') dir2 =1;
+        else dir2=0;
+        x += dx[dir2]*dist[i];
+        y += dy[dir2]*dist[i];
     }
+    
     cout << x<<" "<<y;
 
-    // Please write your code here.
 
     return 0;
 }

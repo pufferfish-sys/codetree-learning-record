553f6ecfa6c6efe8d2483a1832ce873177983da4
pufferfish-sys <sonbal7762@gmail.com>
2025-10-16 13:20:33 +0900

    Title: 연속되는 수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251016/연속되는 수 2/continuous-number2.cpp
+++ 251016/연속되는 수 2/continuous-number2.cpp
@@ -11,8 +11,8 @@ int main() {
         cin >> arr[i];
     }
     for(int i = 0; i<N;i++){
-        if(arr[i]==arr[i-1]||i==0) cnt++;
-        else {maxcnt=max(maxcnt,cnt);cnt=1;} 
+        if(arr[i]==arr[i-1]||i==0) {cnt++;maxcnt=max(maxcnt,cnt);}
+        else cnt=1; 
     }
     
     cout << maxcnt;

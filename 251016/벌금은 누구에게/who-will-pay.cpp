25f2924e9c8b9368f22c4ef106dfd3de68b1ddda
pufferfish-sys <sonbal7762@gmail.com>
2025-10-16 16:12:06 +0900

    Title: 벌금은 누구에게, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251016/벌금은 누구에게/who-will-pay.cpp
+++ 251016/벌금은 누구에게/who-will-pay.cpp
@@ -1,33 +1,22 @@
 #include <iostream>
 
-#define MAX_N 100
-#define MAX_M 10000
-
 using namespace std;
 
-int n, m, k;
-int penalized_person[MAX_M];
-int penalty_num[MAX_N + 1];
-
+int N, M, K;
+int student[10000];
+int arr[101]={0,};
 int main() {
-    // 입력
-    cin >> n >> m >> k;
-    for(int i = 0; i < m; i++)
-        cin >> penalized_person[i];
-    
-    // 각 패널티 횟수를 세서,
-    // 최초로 K번 이상 벌칙을 받는 사람을 추적합니다.
-    int ans = -1;
-    for(int i = 0; i < m; i++) {
-        int target = penalized_person[i];
-        penalty_num[target]++;
-        
-        if(penalty_num[target] >= k) {
-            ans = target;
-            break;
-        }
+    cin >> N >> M >> K;
+    int bul=-1;
+    for (int i = 0; i < M; i++) {
+        cin >> student[i];
     }
-    
+    int ans=-1;
+    for(int i=0;i<M;i++){
+        arr[student[i]]++;
+        if(arr[student[i]]>=K) {ans=student[i]; break;}
+    }
     cout << ans;
+
     return 0;
 }

a76e3e3181926dca004a1b529987561d38082cad
pufferfish-sys <sonbal7762@gmail.com>
2025-10-18 01:36:11 +0900

    Title: 격자 위의 편안한 상태, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251018/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
+++ 251018/격자 위의 편안한 상태/comfortable-state-on-the-grid.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
+
 using namespace std;
 
 int N, M;
 int r[10000], c[10000];
-int grid[101][101] = {0,};
-int dx[4] = {-1, 1, 0, 0};
-int dy[4] = {0, 0, -1, 1};
+int grid[100][100]={0,};
+int dx[4] = {0,1,0,-1},dy[4] ={1,0,-1,0};
 
-bool InRange(int x, int y) {
-    return (0 <= x && x <=N && 0 <= y && y <=N);
+bool InRange(int x, int y){
+        return (0 <= x && x < N && 0 <= y && y < N);
 }
 
 int main() {
@@ -16,20 +16,20 @@ int main() {
     for (int i = 0; i < M; i++) {
         cin >> r[i] >> c[i];
     }
-
-    for (int j = 0; j < M; j++) {
-        grid[r[j]][c[j]] = 1; // 이번 칸 채우기
-
-        int cnt = 0;
-        for (int d = 0; d < 4; d++) {
-            int nx = r[j] + dx[d];
-            int ny = c[j] + dy[d];
-            if (InRange(nx, ny) && grid[nx][ny] == 1)
-                cnt++;
-        }
-        if (cnt == 3) cout << 1 << endl;
-        else cout << 0 << endl;
+    for(int j=0;j<M;j++){
+        int cnt=0;
+    grid[r[j]][c[j]]=1;
+    for(int dir_num=0;dir_num <4; dir_num++){
+                int nx = r[j]+dx[dir_num],ny = c[j]+dy[dir_num];
+                if(InRange(nx,ny)&&grid[nx][ny]==1) cnt++;
+            }
+    if(cnt==3) cout<<1<<endl;
+    else cout<<0<<endl;
     }
-
     return 0;
 }
+
+
+
+
+

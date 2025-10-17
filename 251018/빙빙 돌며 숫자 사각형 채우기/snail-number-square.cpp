d3e8a3cc0d9711f9e5b0efcdfb18597e4b2b13c3
pufferfish-sys <sonbal7762@gmail.com>
2025-10-18 00:48:46 +0900

    Title: 빙빙 돌며 숫자 사각형 채우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251018/빙빙 돌며 숫자 사각형 채우기/snail-number-square.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100][100]={};
+int dx[4]={0,1,0,-1},dy[4]={1,0,-1,0};
+int dir_num;
+int x=0,y=0;
+bool InRange(int x,int y){
+    return(0<=x&&x<n&&0<=y&&y<m);
+}
+int main() {
+    cin >> n >> m;
+    arr[x][y]=1;
+    for(int i=2;i<=n*m;i++){
+        int nx = x+dx[dir_num],ny= y+dy[dir_num];
+        if(!InRange(nx,ny)||arr[nx][ny]!=0)
+            dir_num =(dir_num+1)%4;
+            x=x+dx[dir_num];y=y+dy[dir_num];
+            arr[x][y] =i;
+    }
+    for(int i=0;i<n;i++){
+        for(int j=0; j<m;j++)
+            cout <<arr[i][j]<<" ";
+        cout <<endl;            
+    }
+    
+
+
+    // Please write your code here.
+
+    return 0;
+}

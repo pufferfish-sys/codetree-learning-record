6a1fc39bffefb6a56a8dc271b7110db08aca62b9
pufferfish-sys <sonbal7762@gmail.com>
2025-10-19 02:07:29 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251019/이상한 진수 2/awkward-digits-2.cpp
+++ 251019/이상한 진수 2/awkward-digits-2.cpp
@@ -1,32 +1,32 @@
 #include <iostream>
-
+#include <math.h>
 using namespace std;
 
+string a;
+
 int main() {
-    string a;
     cin >> a;
-
-    int max1 = 0, flag = 0;
-
-    // 1. 첫 번째 0을 찾아서 1로 바꾸기
-    for (int j = 1; j < a.size() - 1; j++) {
-        if (a[j] == '0') {
-            flag = 1;
-            a[j] = '1'; // 수정된 부분
+    int max1=0,flag=0,sum=0;
+        for(int j=1;j<a.size();j++){
+            if(a[j]=='0'){
+                flag=1;
+            a[j]='1';
+            for(int k=0;k<a.size();k++){
+                sum+=(a[k]-'0')*pow(2,a.size()-1-k);
+            }
+            max1=sum;
             break;
+            }
         }
-    }
+        if(flag==0){
+        a[a.size()-1]='0';
+        for(int k=0;k<a.size();k++){
+                sum+=(a[k]-'0')*pow(2,a.size()-1-k);
+            }
+            max1=sum;
+            }
+    cout <<max1;
 
-    // 2. 만약 0이 없었다면 -> 마지막 비트를 0으로 변경
-    if (flag == 0) a[a.size() - 1] = '0';
 
-    // 3. 2진수를 10진수로 변환
-    int sum = 0;
-    for (int k = 0; k < a.size(); k++) {
-        sum = sum * 2 + (a[k] - '0');
-    }
-    max1 = sum;
-
-    cout << max1;
     return 0;
 }

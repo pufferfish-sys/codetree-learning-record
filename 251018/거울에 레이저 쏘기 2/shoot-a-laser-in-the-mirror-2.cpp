beda1078e3f840bd9ac8a50db85238c7ed2d5a49
pufferfish-sys <sonbal7762@gmail.com>
2025-10-18 15:00:57 +0900

    Title: 거울에 레이저 쏘기 2, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251018/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
+++ 251018/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.cpp
@@ -9,7 +9,7 @@ int dx[4] = {-1, 0, 1, 0};
 int dy[4] = {0, 1, 0, -1};
 // 0: 북, 1: 동, 2: 남, 3: 서
 int num;
-int cnt=1;
+int cnt=0;
 bool InRange(int x, int y){
         return (0 <= x && x < n && 0 <= y && y < n);
 }
@@ -31,12 +31,16 @@ int main() {
     while (true) {
     if(!InRange(nx,ny)) break; // 먼저 범위 체크
     if(grid[nx][ny]=='/'){
-        if(num==1||num==3) num=(num+1)%4;
-        else num=(num-1+4)%4;
+        if(num==0) num=1;
+        else if(num==1) num=0;
+        else if(num==2) num=3;
+        else num=2;
     }
     else { // '\'
-        if(num==0||num==2) num=(num+1)%4;
-        else num=(num-1+4)%4;
+        if(num==0) num=3;
+        else if(num==1) num=2;
+        else if(num==2) num=1;
+        else num=0; 
     }
     nx += dx[num];
     ny += dy[num];

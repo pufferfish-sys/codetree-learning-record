432c8c64d79e537142fa54c2f0dcbbc3632ff18a
pufferfish-sys <sonbal7762@gmail.com>
2025-10-18 16:14:31 +0900

    Title: 이동경로상에 있는 모든 숫자 더하기, Time: 10ms, Memory: 0MB, Status: Passed - Codetree

--- 251018/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
+++ 251018/이동경로상에 있는 모든 숫자 더하기/add-all-the-numbers-on-the-path.cpp
@@ -1,17 +1,37 @@
 #include <iostream>
 #include <string>
-
 using namespace std;
 
 int N, T;
 string str;
 int board[100][100];
-int dx[4]={-1,0,1,0},dy[4]={0,1,0,-1};
-int num=0;
+int dx[4] = {-1,0,1,0}, dy[4] = {0,1,0,-1};
+int num = 0;
+int x, y, cnt;
 
 bool Range(int x,int y){
-    return(0<=x && x<N && 0<=y && y<N);
+    return (0<=x && x<N && 0<=y && y<N);
 }
+
+void Sim() {
+    for(int i = 0; i < T; i++){
+        if(str[i]=='R'){
+            num = (num+1)%4;
+        }
+        else if(str[i]=='L'){
+            num = (num-1+4)%4;
+        }
+        else{ // 'F'
+            int nx = x + dx[num], ny = y + dy[num];
+            if(Range(nx, ny)){
+                x = nx;
+                y = ny;
+                cnt += board[x][y];
+            }
+        }
+    }
+}
+
 int main() {
     cin >> N >> T;
     cin >> str;
@@ -20,26 +40,12 @@ int main() {
             cin >> board[i][j];
         }
     }
-    int x=N/2,y=N/2;
-    int cnt = board[x][y];
-    for(int i=0; i<T;i++){
-        if(str[i]=='R'){
-            num = (num+1)%4;
-        }
-        else if(str[i]=='L'){
-            num = (num-1+4)%4;
-        }
-        else{
-            int nx = x+dx[num],ny=y+dy[num];
-            if(Range(nx,ny)){
-            x+=dx[num];
-            y+=dy[num];
-            cnt += board[x][y];
-            }
-        }
-    }
+    x = N/2;
+    y = N/2;
 
-    cout<< cnt;
+    cnt = board[x][y];
+    Sim();
+    cout << cnt;
 
     return 0;
 }

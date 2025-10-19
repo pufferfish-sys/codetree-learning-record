46550b986eef2ce9b160d695ff0e822b69c8249a
pufferfish-sys <sonbal7762@gmail.com>
2025-10-20 02:13:28 +0900

    Title: 오목, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251020/오목/O-mok.cpp
+++ 251020/오목/O-mok.cpp
@@ -1,77 +1,65 @@
 #include <iostream>
-
 using namespace std;
 
 int board[19][19];
 
 int main() {
     for (int i = 0; i < 19; i++)
-        for (int j = 0; j < 19; j++) cin >> board[i][j];
+        for (int j = 0; j < 19; j++)
+            cin >> board[i][j];
 
-    for(int i=0; i<19;i++){
-        for(int j=0;j<15;j++){
-            if(board[i][j]==board[i][j+1]&&
-            board[i][j]==board[i][j+2]&&
-            board[i][j]==board[i][j+3]&&
-            board[i][j]==board[i][j+4]&&
-            (board[i][j]==1||board[i][j]==2)){
-                cout<<board[i][j]<<endl;
-                cout<<i+1<<" "<<j+3<<endl;
-                return 0; 
+    // 1️⃣ 가로 검사
+    for (int i = 0; i < 19; i++) {
+        for (int j = 0; j < 15; j++) {
+            int stone = board[i][j];
+            if (stone && stone == board[i][j+1] && stone == board[i][j+2] &&
+                stone == board[i][j+3] && stone == board[i][j+4]) {
+                cout << stone << '\n';
+                cout << i + 1 << ' ' << j + 3 << '\n'; // 중앙
+                return 0;
             }
         }
     }
-    for(int i=0; i<15;i++){
-        for(int j=0;j<19;j++){
-            if(board[j][i]==board[j][i+1]
-            &&board[j][i]==board[j][i+2]
-            &&board[j][i]==board[j][i+3]
-            &&board[j][i]==board[j][i+4]
-            &&(board[j][i]==1||board[j][i]==2)){
-                cout<<board[j][i]<<endl;
-                cout<<j+1<<" "<<i+3<<endl;
-                return 0; 
+
+    // 2️⃣ 세로 검사
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 19; j++) {
+            int stone = board[i][j];
+            if (stone && stone == board[i+1][j] && stone == board[i+2][j] &&
+                stone == board[i+3][j] && stone == board[i+4][j]) {
+                cout << stone << '\n';
+                cout << i + 3 << ' ' << j + 1 << '\n'; // 중앙
+                return 0;
             }
         }
     }
-    for(int i=0; i<15;i++){
-        for(int j=0;j<15;j++){
-            if(board[i][j]==board[i+1][j+1]&&
-            board[i][j]==board[i+2][j+2]&&
-            board[i][j]==board[i+3][j+3]&&
-            board[i][j]==board[i+4][j+4]&&
-            (board[i][j]==1||board[i][j]==2)){
-                cout<<board[i][j]<<endl;
-                cout<<i+3<<" "<<j+3<<endl;
-                return 0; 
+
+    // 3️⃣ 대각선 ↘ (오른쪽 아래)
+    for (int i = 0; i < 15; i++) {
+        for (int j = 0; j < 15; j++) {
+            int stone = board[i][j];
+            if (stone && stone == board[i+1][j+1] && stone == board[i+2][j+2] &&
+                stone == board[i+3][j+3] && stone == board[i+4][j+4]) {
+                cout << stone << '\n';
+                cout << i + 3 << ' ' << j + 3 << '\n'; // 중앙
+                return 0;
             }
         }
     }
-    for(int i=0; i<15;i++){
-        for(int j=0;j<15;j++){
-            if(board[i][j]==board[i+1][j+1]&&
-            board[i][j]==board[i+2][j+2]&&
-            board[i][j]==board[i+3][j+3]&&
-            board[i][j]==board[i+4][j+4]&&
-            (board[i][j]==1||board[i][j]==2)){
-                cout<<board[i][j]<<endl;
-                cout<<i+3<<" "<<j+3<<endl;
-                return 0; 
+
+    // 4️⃣ 대각선 ↗ (오른쪽 위)
+    for (int i = 4; i < 19; i++) {
+        for (int j = 0; j < 15; j++) {
+            int stone = board[i][j];
+            if (stone && stone == board[i-1][j+1] && stone == board[i-2][j+2] &&
+                stone == board[i-3][j+3] && stone == board[i-4][j+4]) {
+                cout << stone << '\n';
+                cout << i - 2 << ' ' << j + 3 << '\n'; // 중앙
+                return 0;
             }
         }
     }
-    for(int i=18;i>=4;i--){
-        for(int j=18;j>=4;j--){
-            if(board[i][j]==board[i-1][j-1]&&
-            board[i][j]==board[i-2][j-2]&&
-            board[i][j]==board[i-3][j-3]&&
-            board[i][j]==board[i-4][j-4]&&
-            (board[i][j]==1||board[i][j]==2)){
-                cout<<board[i][j]<<endl;
-                cout<<i-3<<" "<<j-3<<endl;
-                return 0; 
-            }
-        }
-    }
+
+    cout << 0;
     return 0;
 }

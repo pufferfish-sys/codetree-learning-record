885864073fb4f0909922dc271ca8b564b9016087
pufferfish-sys <sonbal7762@gmail.com>
2025-10-19 16:47:33 +0900

    Title: 씨 오 더블유 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251019/씨 오 더블유 2/c-o-w-2.cpp
+++ 251019/씨 오 더블유 2/c-o-w-2.cpp
@@ -12,9 +12,9 @@ int main() {
     int cnt=0;
     for(int i=0;i<n-2;i++){
         if(str[i]=='C'){
-            for(int j=i;j<n-1;j++){
+            for(int j=i+1;j<n-1;j++){
                 if(str[j]=='O'){
-                    for(int k=j;k<n;k++){
+                    for(int k=j+1;k<n;k++){
                         if(str[k]=='W') cnt++;
                     }
                 }

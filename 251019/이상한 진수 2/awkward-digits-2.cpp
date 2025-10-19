89627e06f827a63f6a39fb692b1390c4532a2b5b
pufferfish-sys <sonbal7762@gmail.com>
2025-10-19 16:41:06 +0900

    Title: 이상한 진수 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251019/이상한 진수 2/awkward-digits-2.cpp
+++ 251019/이상한 진수 2/awkward-digits-2.cpp
@@ -9,22 +9,16 @@ int main() {
     int max1=0,flag=0,num=0;
         for(int j=1;j<a.size();j++){
             if(a[j]=='0'){
-                flag=1;
+            flag=1;
             a[j]='1';
             for(int k=0;k<a.size();k++){
-                num=num*2+(a[k]-'0');
-            }
-            break;
-            }
-        }
-        if(flag==0){
-        a[a.size()-1]='0';
-        for(int k=0;k<a.size();k++){
-                num=num*2+(a[k]-'0');
-            }
-        }
-    cout <<num;
+                num=num*2+(a[k]-'0');}
+            break;}}
+    if(flag==0){
+    a[a.size()-1]='0';
+    for(int k=0;k<a.size();k++)num=num*2+(a[k]-'0');}
 
+    cout <<num;
 
     return 0;
 }

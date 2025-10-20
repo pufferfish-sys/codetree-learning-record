30bfb52b024364327ff37913972c894196f00338
pufferfish-sys <sonbal7762@gmail.com>
2025-10-20 15:58:04 +0900

    Title: 최고의 13위치 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251020/최고의 13위치 2/best-place-of-13-2.cpp
+++ 251020/최고의 13위치 2/best-place-of-13-2.cpp
@@ -6,7 +6,7 @@ int N;
 int arr[20][20];
 int ma=0;
 bool range(int x,int y){
-    return x>=0&&x<N&&y>=0&&y<N;
+    return x>=0&&x<N&&y>=0&&y+2<N;
 }
 int main() {
     cin >> N;
@@ -14,20 +14,18 @@ int main() {
         for (int j = 0; j < N; j++) cin >> arr[i][j];
     for(int i=0;i<N;i++){
         for(int j=0;j<N-2;j++){
-            while(1){
                 int x1 =i,y1=j,cnt=0; 
-                if(!range(x1,y1+2)) break;
+                if(!range(x1,y1)) continue;
                 cnt += arr[x1][y1]+ arr[x1][y1+1]+arr[x1][y1+2];
                 for(int k=0;k<N;k++){
                     for(int l=0;l<N-2;l++){
                     int cnt2=0;
-                    if(k==x1 && l==y1 || k==x1&&l==y1 && k==x1&&l==y1+2) continue;
-                    if(!range(k,l+2)) continue;
+                    if(k == x1 && !(l + 2 < y1 || l > y1 + 2)) continue;
+                    if(!range(k,l)) continue;
                     cnt2 += arr[k][l] + arr[k][l+1] + arr[k][l+2];
                     ma = max(cnt2+cnt,ma);
                     }
                 }
-            break;}
         }
     }
     cout << ma;

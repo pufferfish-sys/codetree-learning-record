22d585e50711c6329d4cfc948f9acfe231ec7190
pufferfish-sys <sonbal7762@gmail.com>
2025-10-21 01:40:03 +0900

    Title: 특정 구간의 원소 평균값, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251021/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
+++ 251021/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+
 using namespace std;
 
 int n;
@@ -9,28 +10,26 @@ int main() {
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-
-    int cnt2 = 0;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = i; j < n; j++) {
-            int sum = 0;
-            int cnt = 0;
-            for (int k = i; k <= j; k++) {
+    int cnt2 =0;
+    for(int i=0;i<n;i++){
+        for(int j=i;j<n;j++){
+            int sum =0,cnt=0;
+            for(int k=i; k<=j;k++){
                 sum += arr[k];
                 cnt++;
             }
-            if (sum % cnt != 0) continue; // 평균이 정수가 아니면 패스
-            int avg = sum / cnt;
-            for (int k = i; k <= j; k++) {
-                if (arr[k] == avg) {
+
+            double avg= (double) sum/cnt;
+
+            for(int k=i; k<=j;k++){
+                if(arr[k]==avg){
                     cnt2++;
-                    break;
-                }
+                    break;}
             }
         }
+        
+        
     }
-
     cout << cnt2;
     return 0;
 }

d2cc590de446b919bbae232523bd951aa5a045a1
pufferfish-sys <sonbal7762@gmail.com>
2025-10-21 14:55:40 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251021/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 251021/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -3,21 +3,25 @@
 using namespace std;
 
 int N, K;
-int candy[100], pos[100];
-int loc[100];
+int candy[1001], pos[1001];
+int loc[1001];  // 위치별 캔디 합
 
 int main() {
     cin >> N >> K;
     int ma = 0;
+    int maxPos = 0;
 
+    // 입력
     for (int i = 0; i < N; i++) {
         cin >> candy[i] >> pos[i];
-        loc[pos[i]] += candy[i];
+        loc[pos[i]] += candy[i];  // 같은 위치 중복 누적
+        maxPos = max(maxPos, pos[i]);
     }
 
-    for (int i = 0; i < 100; i++) {
+    // 각 위치 i를 중심으로 K 범위 내 합 계산
+    for (int i = 0; i <= maxPos; i++) {
         int cnt = 0;
-        for (int j = max(0, i - K); j <= min(99, i + K); j++) {
+        for (int j = max(0, i - K); j <= min(maxPos, i + K); j++) {
             cnt += loc[j];
         }
         ma = max(ma, cnt);

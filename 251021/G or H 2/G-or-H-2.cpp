3431d587ca7fee8bbffbb1eda5b4ed623a941730
pufferfish-sys <sonbal7762@gmail.com>
2025-10-21 15:20:57 +0900

    Title: G or H 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251021/G or H 2/G-or-H-2.cpp
+++ 251021/G or H 2/G-or-H-2.cpp
@@ -16,18 +16,19 @@ int main() {
         minpos= min(minpos,pos[i]);
     }
     int maxpic=0;
-    for(int i=minpos;i<maxpos;i++){
+    for(int i=minpos;i<=maxpos;i++){
         int gcnt=0, hcnt=0, lastloc=0, firstloc=0;
         if(arr[i]=='\0') continue;
         firstloc = i;
-        for(int j=i;j<maxpos;j++){
-            if(arr[j]=='G') {gcnt++;lastloc=j;} 
-            else if(arr[j]=='H') {hcnt++;lastloc=j;}
-        }
+        for(int j=i;j<=maxpos;j++){
+        if(arr[j]=='G') {gcnt++;lastloc=j;} 
+        else if(arr[j]=='H') {hcnt++;lastloc=j;}
         if(gcnt==hcnt||(gcnt==0&&hcnt>0)||(hcnt==0&&gcnt>0)){
             maxpic = max(maxpic,lastloc -firstloc);
+            }
         }
     }
+    
     cout << maxpic;
 
     return 0;

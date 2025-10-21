d8d53991717dd2aa08a258d376fb2a8f84810612
pufferfish-sys <sonbal7762@gmail.com>
2025-10-21 16:34:10 +0900

    Title: 바구니 안의 사탕 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251021/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 251021/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -20,12 +20,11 @@ int main() {
     
     // 모든 구간의 시작점을 잡아봅니다.
     int max_sum = 0;
-    for(int i = 0; i <= 100; i++) {
+    for(int i = 0; i <= 100-k; i++) {
         // 해당 구간 내 원소의 합을 구합니다.
         int sum = 0;
         for(int j = i - k; j <= i + k; j++)
-            if(j >= 0 && j <= 100)
-                sum += arr[j];
+            if(j >= 0) sum += arr[j];
         max_sum = max(max_sum, sum);
     }
                         

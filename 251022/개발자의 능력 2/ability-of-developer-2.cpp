8a55ee65f5c7b0650bc772d727e8e3201b66de04
pufferfish-sys <sonbal7762@gmail.com>
2025-10-22 20:10:03 +0900

    Title: 개발자의 능력 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251022/개발자의 능력 2/ability-of-developer-2.cpp
+++ 251022/개발자의 능력 2/ability-of-developer-2.cpp
@@ -1,46 +1,40 @@
 #include <iostream>
 #include <algorithm>
-
 using namespace std;
 
 int ability[6];
 
-int main() {//123456
-    int sum = 0;
+int main() {
+    for (int i = 0; i < 6; i++) cin >> ability[i];
+
+    int min_diff = 3000001;
+
+    // 6개 중 3쌍 (i,j), (k,l), (a,b)
     for (int i = 0; i < 6; i++) {
-        cin >> ability[i];
-        sum += ability[i];
-    }
-    sort(ability,ability+6);
-    
-    int min_all=3000001;
-    for(int i=0; i<5;i++){
-        for(int j=i+1;j<6;j++){
-        	for(int k=0; k<5;k++){
-				if(k==i) continue;
-        		for(int l=k+1;l<6;l++){
-					if(l==j) continue;
-					for(int a=0; a<5;a++){
-						if(a==i||a==k) continue;
-        				for(int b=a+1;b<6;b++){
-							if(b==j||b==l) continue;
-							int max1 =0,max2=0,min1=3000001,min2=3000001;
-							int sum1=ability[i]+ability[j];
-							int sum2=ability[k]+ability[l];
-							int sum3=ability[a]+ability[b];
-							max1 = max(sum1,sum2);
-							max2 = max(max1,sum3);
-							min1 = min(sum1,sum2);
-							min2 = min(min1,sum3);
-							min_all = min(min_all,max2-min2);
-					}
-    }
-					}
-    }
+        for (int j = i + 1; j < 6; j++) {
+            for (int k = 0; k < 6; k++) {
+                if (k == i || k == j) continue;
+                for (int l = k + 1; l < 6; l++) {
+                    if (l == i || l == j) continue;
+                    for (int a = 0; a < 6; a++) {
+                        if (a == i || a == j || a == k || a == l) continue;
+                        for (int b = a + 1; b < 6; b++) {
+                            if (b == i || b == j || b == k || b == l) continue;
+
+                            int sum1 = ability[i] + ability[j];
+                            int sum2 = ability[k] + ability[l];
+                            int sum3 = ability[a] + ability[b];
+
+                            int max_sum = max({sum1, sum2, sum3});
+                            int min_sum = min({sum1, sum2, sum3});
+                            min_diff = min(min_diff, max_sum - min_sum);
+                        }
+                    }
+                }
+            }
         }
     }
-	cout << min_all;
-   
 
+    cout << min_diff;
     return 0;
 }

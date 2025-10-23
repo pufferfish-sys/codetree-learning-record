168fc902a43af4eed27c1fa686cd56a8a2a2c714
pufferfish-sys <sonbal7762@gmail.com>
2025-10-23 15:17:59 +0900

    Title: 선분 3개 지우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251023/선분 3개 지우기/remove-three-segments.cpp
+++ 251023/선분 3개 지우기/remove-three-segments.cpp
@@ -1,34 +1,39 @@
 #include <iostream>
-
 using namespace std;
 
 int n;
 int l[10], r[10];
+
 int main() {
     cin >> n;
-
     for (int i = 0; i < n; i++) {
         cin >> l[i] >> r[i];
     }
-    int cnt=0;
+
+    int cnt = 0;
     for(int i=0; i<n; i++){
-        for(int j=i+1; j<n;j++){
-            for(int k=j+1;k<n;k++){
-                int arr[100]={0,};
-                for(int a=0;a<n;a++){
-                    if(a==i||a==j||a==k) continue;
-                    for(i=l[a];i<=r[a];i++){
-                        arr[i]++;
+        for(int j=i+1; j<n; j++){
+            for(int k=j+1; k<n; k++){
+                int arr[100] = {0};
+                for(int a=0; a<n; a++){
+                    if(a==i || a==j || a==k) continue;
+                    for(int p=l[a]; p<=r[a]; p++){
+                        arr[p]++;
                     }
                 }
-                for(int b=0;b<100;b++){
-                    if(arr[b]>2) break;
+                bool ok = true;
+                for(int b=0; b<100; b++){
+                    if(arr[b] >= 2){
+                        ok = false;
+                        break;
+                    }
                 }
-                cnt++;
+
+                if(ok) cnt++;
             }
         }
     }
-   cout <<cnt;
 
+    cout << cnt;
     return 0;
 }

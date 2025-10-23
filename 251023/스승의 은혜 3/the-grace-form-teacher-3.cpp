e53b937b90af1d0991c9bc9e365c9972e66916bc
pufferfish-sys <sonbal7762@gmail.com>
2025-10-23 15:39:30 +0900

    Title: 스승의 은혜 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251023/스승의 은혜 3/the-grace-form-teacher-3.cpp
+++ 251023/스승의 은혜 3/the-grace-form-teacher-3.cpp
@@ -3,32 +3,33 @@
 using namespace std;
 
 int N, B;
-int P[1000];
-int S[1000];
-int sum[1000];
+int P[1000], S[1000];
+
 int main() {
     cin >> N >> B;
-
-    for (int i = 0; i < N; i++) {
+    for(int i=0;i<N;i++)
         cin >> P[i] >> S[i];
-        sum[i] = P[i] + S[i];
-    }
-    sort(sum,sum+N);
-    int cnt=0;
 
-    for(int i=0;i<N;i++){
-        B-=sum[i]; 
-        if (B >= 0) cnt++;
-        else if(B<=0){
-            int maxp=0;
-            for(int j=0;j<=i+1;j++){
-                maxp=max(maxp,P[j]);
-            }
-            B+=maxp/2;
-            if(B>=0) {cnt++;break;}
-            else {break;}
+    int ans = 0;
+    for(int coupon=0; coupon<N; coupon++){ // 쿠폰 적용 학생 선택
+        int budget = B;
+        int cost[1000];
+        for(int i=0;i<N;i++){
+            if(i==coupon) cost[i] = P[i]/2 + S[i]; // 쿠폰 적용
+            else cost[i] = P[i] + S[i];
         }
+
+        sort(cost, cost+N); // 최소 비용 순으로 처리
+        int cnt=0;
+        for(int i=0;i<N;i++){
+            if(budget >= cost[i]){
+                budget -= cost[i];
+                cnt++;
+            } else break;
+        }
+        ans = max(ans, cnt);
     }
-    
-    cout << cnt;
+
+    cout << ans;
+    return 0;
 }

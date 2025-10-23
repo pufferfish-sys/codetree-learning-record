5f49da5a81bb675192609fc2f8cf9e352beb0f25
pufferfish-sys <sonbal7762@gmail.com>
2025-10-23 14:28:01 +0900

    Title: 스승의 은혜 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251023/스승의 은혜 2/the-grace-form-teacher-2.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N, B;
+int P[1000];
+
+int main() {
+    cin >> N >> B;
+
+    for (int i = 0; i < N; i++) {
+        cin >> P[i];
+    }
+    sort(P,P+N);
+    int cnt=0;
+    for(int i=0;i<N;i++){
+        B-=P[i];
+        if(B<=0){
+            B+=P[i];
+            P[i]/=2;
+            B-=P[i];
+            if(B<=0) {cout << cnt; return 0;}
+        }
+        cnt++;
+    }
+    cout << 0;
+
+   
+
+    return 0;
+}

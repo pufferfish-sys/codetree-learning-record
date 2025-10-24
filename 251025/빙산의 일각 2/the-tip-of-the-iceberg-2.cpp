8f79ef4086655678adb58dcb9294c250f1dfc6db
pufferfish-sys <sonbal7762@gmail.com>
2025-10-25 01:55:54 +0900

    Title: 빙산의 일각 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
+++ 251025/빙산의 일각 2/the-tip-of-the-iceberg-2.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <algorithm>
 using namespace std;
 
 int n;
@@ -7,24 +7,26 @@ int h[100];
 
 int main() {
     cin >> n;
+    for (int i = 0; i < n; i++) cin >> h[i];
 
-    for (int i = 0; i < n; i++) {
-        cin >> h[i];
-    }
-    int max1=0;
-   for(int i=1;i<=1000;i++){
-    int cnt=0;
-    for(int j =0;j<n;j++){
-        if(h[j]>i){
-            cnt++;
-    for(int k=j+1;k<n;k++){
-        if(h[k]<=i){
-            j=k; break;}
+    int max1 = 0;
+    for (int i = 1; i <= 1000; i++) {
+        int cnt = 0;
+        bool inBlock = false;
+
+        for (int j = 0; j < n; j++) {
+            if (h[j] > i) {
+                if (!inBlock) {
+                    cnt++;         // 새로운 블록 시작
+                    inBlock = true;
+                }
+            } else {
+                inBlock = false;   // 블록 끝
+            }
         }
-        }
-     }
-    max1=max(max1,cnt);
+        max1 = max(max1, cnt);
     }
+
     cout << max1;
     return 0;
 }

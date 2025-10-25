5e881108f5059ac9b02711c6910b219b95707b39
pufferfish-sys <sonbal7762@gmail.com>
2025-10-25 23:55:07 +0900

    Title: 3개의 선 2, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/3개의 선 2/three-lines-2.cpp
+++ 251025/3개의 선 2/three-lines-2.cpp
@@ -1,24 +1,83 @@
 #include <iostream>
+#include <algorithm>
+
+#define MAX_N 20
+#define MAX_X 10
+
 using namespace std;
 
 int n;
-int x[20], y[20];
-int x_n[101] = {0,}, y_n[101] = {0,};
+int x[MAX_N], y[MAX_N];
 
 int main() {
+    // 입력
     cin >> n;
-    for (int i = 0; i < n; i++) {
+
+    for(int i = 0; i < n; i++)
         cin >> x[i] >> y[i];
-        x_n[x[i]]++;
-        y_n[y[i]]++;
-    }
 
-    int cnt = 0;
-    for (int i = 0; i < n; i++) {
-        if (x_n[x[i]] < 2 && y_n[y[i]] < 2)
-            cnt++;
-    }
+    int ans = 0;
 
-    cout << (cnt >= 3 ? 0 : 1);
+    // 모든 직선에 대해 전부 시도해 봅니다.
+    for(int i = 0; i <= MAX_X; i++)
+        for(int j = 0; j <= MAX_X; j++)
+            for(int k = 0; k <= MAX_X; k++) {
+                // success : 직선 3개로 모든 점을 지나게 할 수 있으면 true
+                bool success = true;
+                // x축에 평행한 직선 3개로
+                // 모든 점을 지나게 할 수 있는 경우
+                for(int l = 0; l < n; l++) {
+                    // 해당 점이 직선에 닿으면 넘어갑니다
+                    if(x[l] == i || x[l] == j || x[l] == k)
+                        continue;
+                    
+                    success = false;
+                }
+                if(success)
+                    ans = 1;
+
+                // x축에 평행한 직선 2개와 y축에 평행한 직선 1개로
+                // 모든 점을 지나게 할 수 있는 경우
+                success = true;
+                for(int l = 0; l < n; l++) {
+                    // 해당 점이 직선에 닿으면 넘어갑니다
+                    if(x[l] == i || x[l] == j || y[l] == k)
+                        continue;
+                    
+                    success = false;
+                }
+                if(success)
+                    ans = 1;
+                
+                // x축에 평행한 직선 1개와 y축에 평행한 직선 2개로
+                // 모든 점을 지나게 할 수 있는 경우
+                success = true;
+                for(int l = 0; l < n; l++) {
+                    // 해당 점이 직선에 닿으면 넘어갑니다
+                    if(x[l] == i || y[l] == j || y[l] == k)
+                        continue;
+                    
+                    success = false;
+                }
+                if(success)
+                    ans = 1;
+                
+                // y축에 평행한 직선 3개로
+                // 모든 점을 지나게 할 수 있는 경우
+                success = true;
+                for(int l = 0; l < n; l++) {
+                    // 해당 점이 직선에 닿으면 넘어갑니다
+                    if(y[l] == i || y[l] == j || y[l] == k)
+                        continue;
+                    
+                    success = false;
+                }
+                if(success)
+                    ans = 1;
+                
+            }
+
+    cout << ans;
+    
     return 0;
 }

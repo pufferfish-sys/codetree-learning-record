0b8f8400e94e3007f55c26bea91cf3d4e539f862
pufferfish-sys <sonbal7762@gmail.com>
2025-10-26 02:11:29 +0900

    Title: 독서실의 거리두기 5, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251026/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 251026/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -1,31 +1,36 @@
 #include <iostream>
 #include <string>
-
+#include <algorithm>
 using namespace std;
 
-int N;
-string seat;
-
 int main() {
-    cin >> N;
-    cin >> seat;
-    int maxdis=0;
+    int N;
+    string seat;
+    cin >> N >> seat;
 
-        for(int j=0;j<N;j++){
-            if(seat[j]!='0') continue;
-            int distance1=N, distance2=N;
-            for(int k=j+1;k<N;k++){
-                if(seat[k]=='1') {distance1 = k-j;break;}
+    int maxDist = 0;
+
+    for(int i=0; i<N; i++){
+        if(seat[i]=='0'){
+            seat[i] = '1'; // 새 사람 배치
+
+            int minDist = N; // 전체 최소 거리 초기화
+            int last = -1;
+
+            for(int j=0; j<N; j++){
+                if(seat[j]=='1'){
+                    if(last != -1){
+                        minDist = min(minDist, j - last);
+                    }
+                    last = j;
+                }
             }
-            for(int k=j-1;k>=0;k--){
-                if(seat[k]=='1') {distance2 = j-k; break;}
-            }
-            int mindis = min(distance1,distance2);
-            maxdis = max(mindis,maxdis);
+
+            maxDist = max(maxDist, minDist);
+            seat[i] = '0'; // 원래대로 되돌리기
         }
-    
-    cout << maxdis;
-    
+    }
 
+    cout << maxDist << endl;
     return 0;
 }

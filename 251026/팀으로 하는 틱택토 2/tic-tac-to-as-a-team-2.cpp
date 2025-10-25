64fa0cb9d535cfe0540175d4c21b8a98e02509dd
pufferfish-sys <sonbal7762@gmail.com>
2025-10-26 00:52:15 +0900

    Title: 팀으로 하는 틱택토 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251026/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
+++ 251026/팀으로 하는 틱택토 2/tic-tac-to-as-a-team-2.cpp
@@ -10,11 +10,11 @@ int main() {
     for (int i = 0; i < 3; i++) cin >> inp[i];
 
     int cnt = 0;
-
+    
     // 팀 숫자 조합 i, j (1~9)
     for (int i = 1; i <= 9; i++) {
         for (int j = i + 1; j <= 9; j++) {
-
+            int flag=0;
             // 가로 검사
             for (int k = 0; k < 3; k++) {
                 int cnt_i = 0, cnt_j = 0;
@@ -22,34 +22,35 @@ int main() {
                     if (inp[k][l] == i + '0') cnt_i++;
                     if (inp[k][l] == j + '0') cnt_j++;
                 }
-                if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;break;} // 두 숫자 모두 존재하면 팀 승리
+                if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;flag=1;break;} // 두 숫자 모두 존재하면 팀 승리
             }
 
-            // 세로 검사
+            if(flag) break;
             for (int l = 0; l < 3; l++) {
                 int cnt_i = 0, cnt_j = 0;
                 for (int k = 0; k < 3; k++) {
                     if (inp[k][l] == i + '0') cnt_i++;
                     if (inp[k][l] == j + '0') cnt_j++;
                 }
-                if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;break;}
+                if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;flag=1;break;}
             }
-
+             if(flag) break;
             // 대각선 검사 (왼쪽 위 → 오른쪽 아래)
             int cnt_i = 0, cnt_j = 0;
             for (int d = 0; d < 3; d++) {
                 if (inp[d][d] == i + '0') cnt_i++;
                 if (inp[d][d] == j + '0') cnt_j++;
             }
-            if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;break;}
-
+            if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;flag=1;break;}
+            if(flag) break;
             // 대각선 검사 (오른쪽 위 → 왼쪽 아래)
             cnt_i = 0; cnt_j = 0;
             for (int d = 0; d < 3; d++) {
                 if (inp[d][2 - d] == i + '0') cnt_i++;
                 if (inp[d][2 - d] == j + '0') cnt_j++;
             }
-            if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;break;}
+            if (cnt_i + cnt_j >= 3&& cnt_i>0&&cnt_j>0) {cnt++;flag=1;break;}
+            if(flag) break;
         }
     }
 

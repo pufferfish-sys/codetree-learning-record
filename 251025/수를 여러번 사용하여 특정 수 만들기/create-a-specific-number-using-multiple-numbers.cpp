b1cd71660b8f82bcc5cb3d694deacbb48e335e28
pufferfish-sys <sonbal7762@gmail.com>
2025-10-25 23:29:45 +0900

    Title: 수를 여러번 사용하여 특정 수 만들기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
+++ 251025/수를 여러번 사용하여 특정 수 만들기/create-a-specific-number-using-multiple-numbers.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-
 using namespace std;
 
 int A, B, C;
 
 int main() {
     cin >> A >> B >> C;
-    int ma=0;
-    for(int i=0;i<1000;i++){
-        for(int j=0;j<1000;j++){
-            int sum = A*i + B*j;
-            if(sum<=C){
-                ma=max(sum,ma);
-            }
+    int ma = 0;
+    for (int i = 0; i*A <= C; i++) {      // i의 최대값은 C/A
+        for (int j = 0; j * B <= (C - A * i); j++) {  // A*i가 고정되면 j의 최대는 (C - A*i)/B
+            int sum = A * i + B * j;
+            ma = max(ma, sum);
         }
     }
     cout << ma;

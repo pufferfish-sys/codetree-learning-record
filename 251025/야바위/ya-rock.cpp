6988861c68b6943b77a2100c92924260e53bca76
pufferfish-sys <sonbal7762@gmail.com>
2025-10-25 18:31:39 +0900

    Title: 야바위, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251025/야바위/ya-rock.cpp
+++ 251025/야바위/ya-rock.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+
 using namespace std;
 
 int N;
@@ -10,32 +11,22 @@ int main() {
     for (int i = 0; i < N; i++) {
         cin >> a[i] >> b[i] >> c[i];
     }
-
-    int ma[4] = {0,};
-
-    for (int i = 1; i <= 3; i++) {
-        int cnt = 0;
-        int stone[4] = {0,};
-        stone[i] = 1;
-
-        for (int j = 0; j < N; j++) {
+    int max1=0;
+    for(int i=1;i<=3;i++){
+        int cnt=0;
+        int stone[4]={0,};
+        stone[i]=1;
+        for(int j=0;j<N;j++){
             int temp = stone[b[j]];
-            stone[b[j]] = stone[a[j]];
-            stone[a[j]] = temp;
-            if (stone[c[j]] == 1) cnt++;
+            stone[b[j]]=stone[a[j]];
+            stone[a[j]]=temp;
+            if(stone[c[j]]==1) cnt++;
         }
-        ma[i] = cnt;
+        max1=max(cnt,max1);
     }
+    cout << max1;
 
-    // 최댓값 구하기
-    int max_val = max({ma[1], ma[2], ma[3]});
-
-    // 그 최댓값을 가진 초기 위치 출력
-    for (int i = 1; i <= 3; i++) {
-        if (ma[i] == max_val) {
-            cout << i << '\n';
-        }
-    }
+    // Please write your code here.
 
     return 0;
 }

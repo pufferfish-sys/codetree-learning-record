07a3b191b7f628994f5be599cf7098d845cd090e
pufferfish-sys <sonbal7762@gmail.com>
2025-10-26 01:35:51 +0900

    Title: 원소 값들의 최대 합, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251026/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
+++ 251026/원소 값들의 최대 합/maximum-sum-of-element-values.cpp
@@ -11,7 +11,7 @@ int main() {
         cin >> arr[i];
     }
     int max1=0;
-    for(int j=1;j<=100;j++){
+    for(int j=1;j<=n;j++){
         int start=j,sum=0;
     for(int i=0;i<m;i++){
         sum+=arr[start];

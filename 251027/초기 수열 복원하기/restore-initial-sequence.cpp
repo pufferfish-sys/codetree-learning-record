d05fba716d102a503f8fbca74927e705fac37895
pufferfish-sys <sonbal7762@gmail.com>
2025-10-27 23:14:17 +0900

    Title: 초기 수열 복원하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251027/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 251027/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -22,7 +22,7 @@ int main() {
         for(int i=2;i<n;i++){
               arr[i] = adjacent[i-1]-arr[i-1];
               check[arr[i]]++;
-              if(arr[i]<1) {
+              if(arr[i]<1||arr[i]<=n) {
                 flag = false;
                 break;}   
         }

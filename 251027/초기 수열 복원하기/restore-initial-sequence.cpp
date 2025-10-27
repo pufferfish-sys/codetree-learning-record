14a2f8d5645ce64c3342074c2ca12ac2cb939ac8
pufferfish-sys <sonbal7762@gmail.com>
2025-10-27 23:10:57 +0900

    Title: 초기 수열 복원하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251027/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int adjacent[1000];
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n - 1; i++) {
+        cin >> adjacent[i];
+    }
+
+    for(int x=1;x<=1000;x++){
+        int arr[1001];
+        int check[1000]={0,};
+        bool flag = true;
+        arr[0]= x; 
+        arr[1]= adjacent[0]-arr[0];
+        check[arr[0]]++; 
+        check[arr[1]]++;
+        for(int i=2;i<n;i++){
+              arr[i] = adjacent[i-1]-arr[i-1];
+              check[arr[i]]++;   
+        }
+        for(int y=1;y<=n;y++){
+            if(check[y]>1) {
+                flag = false;
+                break;
+            }
+        }
+        if(flag) 
+        {for(int z=0;z<n;z++) cout<<arr[z]<<" ";
+        break;
+        }
+    }
+    
+    
+
+    return 0;
+}

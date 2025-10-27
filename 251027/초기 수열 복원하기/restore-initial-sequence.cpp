67d942c285c2a1c0f3d123a3d34c09c03b4cfd1b
pufferfish-sys <sonbal7762@gmail.com>
2025-10-27 23:25:50 +0900

    Title: 초기 수열 복원하기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251027/초기 수열 복원하기/restore-initial-sequence.cpp
+++ 251027/초기 수열 복원하기/restore-initial-sequence.cpp
@@ -18,23 +18,16 @@ int main() {
         arr[0]= x; 
         check[arr[0]]++; 
         for(int i=1;i<n;i++){
-              arr[i] = adjacent[i-1]-arr[i-1];
+            arr[i] = adjacent[i-1]-arr[i-1];
             if(arr[i] < 1 || arr[i] > n) { flag = false; break; }  
-              check[arr[i]]++;
-        }
-        for(int y=1;y<=n;y++){
-            if(check[y]>1) {
-                flag = false;
-                break;
-            }
+            check[arr[i]]++;
+            if(check[arr[i]]>1) {flag = false; break;}
         }
         if(flag) 
         {for(int z=0;z<n;z++) cout<<arr[z]<<" ";
         break;
         }
     }
-    
-    
 
     return 0;
 }

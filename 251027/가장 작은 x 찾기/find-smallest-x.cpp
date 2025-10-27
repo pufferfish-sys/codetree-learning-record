5a3c4d8c8b84d0c5a9a27d6c74e8fecbc26fa83f
pufferfish-sys <sonbal7762@gmail.com>
2025-10-27 17:59:23 +0900

    Title: 가장 작은 x 찾기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251027/가장 작은 x 찾기/find-smallest-x.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int n;
+int a[10], b[10];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> a[i] >> b[i];
+    }
+    int ans = INT_MAX;
+    for(int i=1;i<=10000;i++){
+        int start=i*2; bool flag = true;
+    for(int j=0;j<n;j++){    
+        if(start>=a[j]&&start<=b[j]) {start*=2; continue;}
+        else{flag = false; break;} 
+    }
+    if(flag) ans=min(ans,i);
+    }
+    cout << ans;
+    return 0;
+}

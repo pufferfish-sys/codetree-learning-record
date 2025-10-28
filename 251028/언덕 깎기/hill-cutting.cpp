80fa47d3b9084e26a5c30cb7b118ae0410715d30
pufferfish-sys <sonbal7762@gmail.com>
2025-10-28 16:29:05 +0900

    Title: 언덕 깎기, Time: 9ms, Memory: 0MB, Status: Passed - Codetree

--- 251028/언덕 깎기/hill-cutting.cpp
+++ 251028/언덕 깎기/hill-cutting.cpp
@@ -3,44 +3,27 @@
 using namespace std;
 
 int N;
-int heights[1000];
-int cntlow=0,cnthigh=0;
+int h[1000];
+
 int main() {
     cin >> N;
-    for (int i = 0; i < N; i++) {
-        cin >> heights[i];
-    }
-    while(1){
-    int cnt1=0,cnt2=0;
-    int min1=101,max1=0; 
-    for(int i = 0; i < N; i++){
-        min1=min(min1,heights[i]);
-    }
-    for(int i = 0; i < N; i++){
-        max1=max(max1,heights[i]);
-    }
-    int diff=max1-min1;
-    if(diff<=17) break;
-    for(int i = 0; i < N; i++){
-       if(heights[i]==min1) cnt1++;
-       else if(heights[i]==max1) cnt2++;
+    for (int i = 0; i < N; i++) cin >> h[i];
+
+    int ans = 10000000;
+
+    
+    for (int L = 0; L <= 83; L++) {
+        int R = L + 17;
+        int cost = 0;
+
+        for (int i = 0; i < N; i++) {
+            if (h[i] < L) cost += (L - h[i]) * (L - h[i]);
+            else if (h[i] > R) cost += (h[i] - R) * (h[i] - R);
+        }
+
+        ans = min(ans, cost);
     }
 
-    if(cnt1<cnt2){
-        cntlow+=cnt1;
-        for(int i = 0; i < N; i++){
-            if(heights[i]==min1) heights[i]++;
-        }
-    }
-    else{
-        cnthigh+=cnt2;
-        for(int i = 0; i < N; i++){
-            if(heights[i]==max1) heights[i]--;
-        }
-    }
-         
-    }
-    int cost = cntlow*cntlow+cnthigh*cnthigh;
-    cout << cost;
+    cout << ans;
     return 0;
 }

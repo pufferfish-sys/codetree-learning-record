20e7dcc09063229fb96f4441441e93a88f44be59
pufferfish-sys <sonbal7762@gmail.com>
2025-10-30 00:57:40 +0900

    Title: 전부 겹치는 선분, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251030/전부 겹치는 선분/overlapping-line-segments.cpp
+++ 251030/전부 겹치는 선분/overlapping-line-segments.cpp
@@ -4,13 +4,18 @@ using namespace std;
 
 int n;
 int x1[100], x2[100];
-int arr[100]={0,};
+int arr[101]={0,};
 int main() {
     cin >> n;
 
     for (int i = 0; i < n; i++) {
         cin >> x1[i] >> x2[i];
-        arr[x1[i]]++; arr[x2[i]]++;
+        
+    }
+    for(int i=0;i<n;i++){
+        for(int j=x1[i];j<=x2[i];j++){
+            arr[j]++;
+        }
     }
     for(int i=1;i<=100;i++){
         if(arr[i]==n) {cout << "Yes"; return 0;}

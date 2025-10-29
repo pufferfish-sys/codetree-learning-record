79feccb6989aab8153f295b60cd2b9b767f439f6
pufferfish-sys <sonbal7762@gmail.com>
2025-10-29 21:55:55 +0900

    Title: 최대 최소간의 차, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251029/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
+++ 251029/최대 최소간의 차/difference-between-maximum-and-minimum.cpp
@@ -12,14 +12,14 @@ int main() {
         cin >> arr[i];
     }
     int ans = INT_MAX;
-    for(int i=0;i<=100-k;i++){
+    for(int i=0;i<=10000-k;i++){
         int cost =0;
         int j = i+k;
         for(int x=0;x<n;x++){
             if(arr[x]>j) cost+= abs(arr[x]-j);
             else if(arr[x]<i) cost+= abs(arr[x]-i);
         }
-        ans =min(cost,ans);
+        ans = min(cost,ans);
         
     }
     cout << ans;

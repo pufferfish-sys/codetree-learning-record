9d325bd4a4f1e082c73cbcea13994f276edfc873
pufferfish-sys <sonbal7762@gmail.com>
2025-10-31 00:56:24 +0900

    Title: 코딩톡, Time: 6ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251031/코딩톡/coding-talk.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m, p;
+char c[100];
+int u[100];
+int check[26]={0,};
+int main() {
+    cin >> n >> m >> p;
+
+    for (int i = 0; i < m; i++) {
+        cin >> c[i] >> u[i];
+    }
+
+    for(int i=p-1;i<m;i++){
+        int d = c[i]-'A';
+        check[d]=1;
+    }
+
+    if(u[p-1]==0){
+        for(int i=0;i<n;i++){
+            check[i]=1;
+        }
+    }
+
+
+    for(int i=0;i<m;i++){
+        char d = 'A'+i;
+        if(check[i]!=1) cout << d << " "; 
+    }
+    
+
+    return 0;
+}

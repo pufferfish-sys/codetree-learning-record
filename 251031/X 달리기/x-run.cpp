7b3e3af808aedc2df39c71a923acb08c3093c95c
pufferfish-sys <sonbal7762@gmail.com>
2025-10-31 01:44:50 +0900

    Title: X 달리기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251031/X 달리기/x-run.cpp
+++ 251031/X 달리기/x-run.cpp
@@ -4,17 +4,26 @@ using namespace std;
 
 int X;
 int cnt=0;
+
 int main() {
     cin >> X;
     int speed=1;
     int time=0;
-    while(cnt<=X/2){
+    while(cnt<X){
         cnt += speed;
-        speed += 1;
+        int sum=0;
+        for(int i=1;i<=speed+1;i++){
+        sum+=i;
+        }
+        if(sum<=X-cnt) speed++;
+        else if(sum-speed-1<=X-cnt){}
+        else speed--;
+
         time++;
     }
 
-    cout << time*2;
+
+    cout << time;
 
     return 0;
 }

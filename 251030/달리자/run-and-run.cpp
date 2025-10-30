5e518f06329b297bd9388eed50827a9b8e3372bf
pufferfish-sys <sonbal7762@gmail.com>
2025-10-30 17:19:41 +0900

    Title: 달리자, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251030/달리자/run-and-run.cpp
+++ 251030/달리자/run-and-run.cpp
@@ -20,12 +20,11 @@ int main() {
     // 입력으로 주어진 사람 수를 보고
     // 최소 어느 만큼의 거리를 달려야 하는지 확인합니다.
     for(int i = 0; i < n; i++) {
-        if(a[i] > b[i]) {
-            // 최소 a[i] - b[i]명의 사람들은 오른쪽으로 달려야 합니다.
-            int num = a[i] - b[i];
+        if(a[i]>b[i]){
+            int num = a[i]-b[i];
             a[i] -= num;
-            a[i + 1] += num;
-            ans += num;
+            a[i+1] += num;
+            ans +=num;
         }
     }
 

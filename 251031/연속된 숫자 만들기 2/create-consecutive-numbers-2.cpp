10bfd1cb402ff6f993ef5c1f25fd0c92037e74a6
pufferfish-sys <sonbal7762@gmail.com>
2025-10-31 21:44:52 +0900

    Title: 연속된 숫자 만들기 2, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251031/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
+++ 251031/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -9,7 +9,7 @@ int main() {
     for (int i = 0; i < 3; i++) cin >> pos[i];
     sort(pos,pos+3);
     if (pos[0]+1==pos[1]&&pos[1]+1 ==pos[2]) cout << 0;
-    else if(pos[0]+1==pos[1]||pos[1]+1==pos[2]) cout << 1;
+    else if(pos[0]+1==pos[1]||pos[1]+1==pos[2]||pos[1]+2==pos[2]||pos[0]+2==pos[2]) cout << 1;
     else cout << 2;
     return 0;
 }

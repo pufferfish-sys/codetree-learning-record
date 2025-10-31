877cb939eed4ed294cb200a5581e9a18e1ff6e60
pufferfish-sys <sonbal7762@gmail.com>
2025-10-31 21:32:39 +0900

    Title: 순 간 이 동, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251031/순 간 이 동/teleportation.cpp
+++ 251031/순 간 이 동/teleportation.cpp
@@ -7,9 +7,9 @@ int a, b, x, y;
 
 int main() {
     cin >> a >> b >> x >> y;
-    int time1 = b-a;
-    int time2 = abs(a-x)+b-y;
-    int time3 = abs(a-y)+b-x;
+    int time1 = abs(b-a);
+    int time2 = abs(a-x)+abs(b-y);
+    int time3 = abs(a-y)+abs(b-x);
     int mint = min({time1,time2,time3});   
     cout << mint;
     

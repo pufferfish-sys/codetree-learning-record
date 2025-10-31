d1050dbf27422f3e09595af17a125adee328a179
pufferfish-sys <sonbal7762@gmail.com>
2025-10-31 22:44:43 +0900

    Title: 독서실의 거리두기 3, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

--- 251031/독서실의 거리두기 3/study-cafe-keeping-distance-3.cpp
+++ 251031/독서실의 거리두기 3/study-cafe-keeping-distance-3.cpp
@@ -1,27 +1,62 @@
 #include <iostream>
 #include <string>
 #include <algorithm>
+#include <climits>
+
 using namespace std;
 
 int n;
-string s;
+string seats;
 
 int main() {
-    cin >> n >> s;
+    // 입력:
+    cin >> n;
+    cin >> seats;
+    
+    // Step1. 최적의 위치 찾기
+    // 인접한 쌍들 중 가장 먼 1간의 쌍을 찾습니다.
+    int max_dist = 0;
+    int max_i, max_j;
+    for(int i = 0; i < n; i++) {
+        if(seats[i] == '1') {
+            for(int j = i + 1; j < n; j++)
+                if(seats[j] == '1') {
+                    // 1간의 쌍을 골랐을 때
+                    // 두 좌석간의 거리가 지금까지의 최적의 답 보다 더 좋다면
+                    // 값을 갱신해줍니다.
+                    if(j - i > max_dist) {
+                        max_dist = j - i;
 
-    int maxNew = 0;
-    for (int i = 0; i < n; i++) {
-        if (s[i] == '1') continue;
+                        // 이때, 두 좌석의 위치를 기억합니다.
+                        max_i = i;
+                        max_j = j;
+                    }
 
-        int l = i - 1;
-        while (l >= 0 && s[l] == '0') l--;
-        int r = i + 1;
-        while (r < n && s[r] == '0') r++;
-
-        int dist = min(i - l, r - i);
-        maxNew = max(maxNew, dist);
+                    // 인접한 쌍을 찾았으므로 빠져나옵니다.
+                    break;
+                }
+        }
     }
 
-    cout << maxNew;
+    // Step2. 최적의 위치에 1을 놓습니다.
+    // 가장 먼 쌍의 위치 가운데에 놓으면 됩니다.
+    seats[(max_i + max_j) / 2] = '1';
+
+    // Step3. 이제 인접한 쌍들 중 가장 가까운 1간의 쌍을 찾습니다.
+    // 이때의 값이 답이 됩니다.
+    int ans = INT_MAX;
+    for(int i = 0; i < n; i++) {
+        if(seats[i] == '1') {
+            for(int j = i + 1; j < n; j++)
+                if(seats[j] == '1') {
+                    ans = min(ans, j - i);
+					
+					// 인접한 쌍을 찾았으므로 빠져나옵니다.
+                    break;
+                }
+        }
+    }
+
+    cout << ans;
     return 0;
 }

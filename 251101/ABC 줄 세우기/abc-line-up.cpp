9bc0e38d8c8b4dcce71844860ebd2bb8cec4692f
pufferfish-sys <sonbal7762@gmail.com>
2025-11-01 23:10:32 +0900

    Title: ABC 줄 세우기, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251101/ABC 줄 세우기/abc-line-up.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+char arr[26];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    int cnt = 0 ;
+    for(int x=0;x<n;x++){
+        for(int i=0;i<n;i++){
+        if(arr[i]=='A'+x){
+            int j = i;
+            while(arr[x]!='A'+x){
+            swap(arr[j],arr[j-1]);
+            cnt ++;
+            j--;
+            }
+        }
+    }
+    }
+    cout << cnt;
+
+    return 0;
+}

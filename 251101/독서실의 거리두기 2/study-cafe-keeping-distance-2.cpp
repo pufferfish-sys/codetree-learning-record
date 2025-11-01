2b3f78e259bb1f672a1d70d07a616f7798a8e165
pufferfish-sys <sonbal7762@gmail.com>
2025-11-01 22:55:15 +0900

    Title: 독서실의 거리두기 2, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251101/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
+++ 251101/독서실의 거리두기 2/study-cafe-keeping-distance-2.cpp
@@ -9,27 +9,24 @@ string seats;
 int main() {
     cin >> N;
     cin >> seats;
-    int ans = 0;
-
-    for(int x = 0; x < N; x++){
-        if(seats[x] == '1') continue;
-        seats[x] = '1';
-
-        int mindis = N;
-        for(int i = 0; i < N-1; i++){
-            if(seats[i] == '0') continue;
-            for(int j = i+1; j < N; j++){
-                if(seats[j] == '1'){
-                    mindis = min(j - i, mindis);
-                    break;
-                }
+    int ans=0;
+    for(int x=0;x<N;x++){
+        if(seats[x]=='1') continue;
+        seats[x]='1';
+        int mindis = 1001;
+        for(int i=0;i<N-1;i++){    
+            if(seats[i]=='0') continue;
+            for(int j=i+1;j<N;j++){
+            if(seats[j]=='1'){
+                mindis = min(j-i,mindis);
+                break;
+            }
             }
         }
-
-        ans = max(ans, mindis); 
-        seats[x] = '0'; 
+        ans = max(mindis,ans);
+        seats[x]='0';
     }
-
+    
     cout << ans;
 
     return 0;

448e4a30162d470d3c2576c6834fb326ad16735f
pufferfish-sys <sonbal7762@gmail.com>
2025-11-03 01:13:56 +0900

    Title: 최소 와이파이 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251103/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+int arr[100];
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int cnt=0;
+
+    for(int i=0;i<n-m;i++){
+        if(arr[i]==1) cnt++;
+        i+=m;
+    }
+    cout << cnt;
+
+    return 0;
+}

3fe06160bed0517ca28848d801719870b8286800
pufferfish-sys <sonbal7762@gmail.com>
2025-11-02 15:44:21 +0900

    Title: 순위 경쟁, Time: 7ms, Memory: 0MB, Status: Passed - Codetree

--- 251102/순위 경쟁/ranking-competition.cpp
+++ 251102/순위 경쟁/ranking-competition.cpp
@@ -20,18 +20,11 @@ int main() {
         else {c1 += s[i];}
 
         int mx = max({a,b,c1});
-        bool aTop = (a == mx);
-        bool bTop = (b == mx);
-        bool cTop = (c1 == mx);
-        int curState;
-
-        if(aTop && !bTop && !cTop) curState = 1;        // A
-        else if(!aTop && bTop && !cTop) curState = 2;   // B
-        else if(!aTop && !bTop && cTop) curState = 3;   // C
-        else if(aTop && bTop && !cTop) curState = 4;    // A=B
-        else if(aTop && !bTop && cTop) curState = 5;    // A=C
-        else if(!aTop && bTop && cTop) curState = 6;    // B=C
-        else curState = 7;                              // A=B=C
+        int curState = 0;
+        if(a == mx) curState+=1;
+        if(b == mx) curState+=2;
+        if(c1 == mx) curState+=4;
+        
         if(status!=curState){
             status = curState;
             cnt++;

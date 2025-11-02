28907271b891a056111a937cc22ac96a8ff17423
pufferfish-sys <sonbal7762@gmail.com>
2025-11-03 01:20:30 +0900

    Title: 최소 와이파이 수, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251103/최소 와이파이 수/minimum-number-of-wifi.cpp
+++ 251103/최소 와이파이 수/minimum-number-of-wifi.cpp
@@ -7,16 +7,19 @@ int arr[100];
 
 int main() {
     cin >> n >> m;
+    int sum=0;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
+        sum+=arr[i];
+        
     }
     int cnt=0;
 
     for(int i=0;i<n-m;i++){
-        if(arr[i]==1) cnt++;
-        i+=m;
+        if(arr[i]==1) {cnt++;i+=m;}
     }
-    if(m>n) cnt=1;
+    if(m>=n) if(sum>=1)cnt=1;
+    else cnt =0;
     cout << cnt;
 
     return 0;

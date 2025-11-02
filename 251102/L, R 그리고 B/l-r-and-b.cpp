1d420dd7cf76b92fe0bf3e03b7a6ec222e3f483e
pufferfish-sys <sonbal7762@gmail.com>
2025-11-02 15:18:09 +0900

    Title: L, R 그리고 B, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

--- 251102/L, R 그리고 B/l-r-and-b.cpp
+++ 251102/L, R 그리고 B/l-r-and-b.cpp
@@ -15,8 +15,8 @@ int main() {
         }
     }
     int cnt=0;
-    if(((loc1==loc3&&loc3==loc5)&&(loc3>loc1))||
-    ((loc2==loc4&&loc4==loc6)&&(loc4>loc2))){
+    if(((loc1==loc3&&loc3==loc5)&&((loc4>loc2&&loc4<loc6)||(loc4<loc2&&loc4>loc2)))||
+    ((loc2==loc4&&loc4==loc6)&&((loc3>loc1&&loc3<loc5)||(loc3<loc1&&loc3>loc5)))){
         cnt=abs(loc1-loc5)+abs(loc2-loc6)+1;
     }
     else{

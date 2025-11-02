18a7ac083fd09044d4ed3d40cd1ba9d9d96ea2b9
pufferfish-sys <sonbal7762@gmail.com>
2025-11-02 16:00:27 +0900

    Title: 세 수의 최대 곱, Time: 8ms, Memory: 0MB, Status: Wrong Answer - Codetree

---
+++ 251102/세 수의 최대 곱/maximum-product-of-three-numbers.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100000];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    // 양음음, 양양양 //전부다 음수일땐 가장 작은거 3개
+    int mx = 0; 
+    for(int x=0; x<n;x++){
+        if(arr[x]>=0) continue;
+        for(int y=0; y<n;y++){
+            if(arr[y]>=0||x==y)continue;
+        for(int z=0; z<n;z++){
+            if(arr[z]<0||z==x||z==y) continue;
+          mx=max(mx,arr[x]*arr[y]*arr[z]);          
+   }
+   }
+
+   }
+    for(int x=0; x<n;x++){
+        if(arr[x]<0) continue;
+        for(int y=0; y<n;y++){
+            if(arr[y]<0||x==y)continue;
+        for(int z=0; z<n;z++){
+            if(arr[z]<0||z==x||z==y) continue;
+          mx=max(mx,arr[x]*arr[y]*arr[z]);          
+   }
+   }
+   }
+
+    cout << mx;
+    return 0;
+}

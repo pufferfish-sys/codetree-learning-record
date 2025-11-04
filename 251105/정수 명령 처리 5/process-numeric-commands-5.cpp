d35157a49074e644505d19ece5f9652ebc2ad843
pufferfish-sys <sonbal7762@gmail.com>
2025-11-05 00:57:26 +0900

    Title: 정수 명령 처리 5, Time: 12ms, Memory: 0MB, Status: Passed - Codetree

--- 251105/정수 명령 처리 5/process-numeric-commands-5.cpp
+++ 251105/정수 명령 처리 5/process-numeric-commands-5.cpp
@@ -1,36 +1,37 @@
 #include <iostream>
 #include <vector>
-#include <string>
 
 using namespace std;
 
 int main() {
+    // 변수 선언 및 입력
     int n;
+
+    // 동적 배열 선언
+    vector<int> v;
+
     cin >> n;
 
-    vector<int> list;
-    string command;
-    int item, index;
-
-    for (int i = 0; i < n; i++) {
+    for(int i = 0; i < n; i++) {
+        string command;
         cin >> command;
 
-        if (command == "push_back") {
-            cin >> item;
-            list.push_back(item);
-        } else if (command == "get") {
+        if(command == "push_back") {
+            int num;
+            cin >> num;
+            v.push_back(num);
+        }
+        else if(command == "pop_back") {
+            v.pop_back();
+        }
+        else if(command == "size") {
+            cout << v.size() << endl;
+        }
+        else {
+            int index;
             cin >> index;
-            if (index >= 1 && index <= list.size()) {
-                cout << list[index - 1] << endl;
-            }
-        } else if (command == "size") {
-            cout << list.size() << endl;
-        } else if (command == "pop_back") {
-            if (!list.empty()) {
-                list.pop_back();
-            }
+            cout << v[index - 1] << endl;
         }
     }
-
     return 0;
 }

a4f0f7b5ecd28abacab0ceca20c99fc38f3440f3
pufferfish-sys <sonbal7762@gmail.com>
2025-11-06 01:44:20 +0900

    Title: 삽입 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251106/삽입 정렬 구현/implement-insertion-sort.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+#define MAX_N 100
+
+using namespace std;
+
+int n;
+int arr[MAX_N];
+void insertion_sort(){
+    for(int i=0; i<n; i++){
+    int j = i - 1; 
+    int key = arr[i];
+    while(j >= 0 && arr[j] > key){
+      arr[j + 1] = arr[j];
+      j--;
+      }
+    arr[j + 1] = key;
+    }
+    }
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    insertion_sort();
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    
+
+    return 0;
+}

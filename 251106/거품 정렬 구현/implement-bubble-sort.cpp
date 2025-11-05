c9bcd8d180bac5198ef0fa056ac4253b71871386
pufferfish-sys <sonbal7762@gmail.com>
2025-11-06 01:14:07 +0900

    Title: 거품 정렬 구현, Time: 8ms, Memory: 0MB, Status: Passed - Codetree

---
+++ 251106/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    int temp;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n-1; j++) {
+            if(arr[j]>arr[j+1]){
+                temp = arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+    for(int i=0; i<n;i++){
+        cout << arr[i] << " ";
+    }
+    
+
+    return 0;
+}

d45d79d266b9952ac31b4c20539636f4a3c662f0
jung_wui <sonbal7762@gmail.com>
2026-09-10 18:03:10 +0900

    [Passed] 두 정수 값 교환하기 (2ms, 0MB)

---
+++ trail2/두 정수 값 교환하기/to-exchange-two-integer-values.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+void swap(int &a, int &b){
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+
+}
+int main() {
+    cin >> n >> m;
+    swap(n,m);
+    cout << n << " " << m;
+    // Please write your code here.
+
+    return 0;
+}

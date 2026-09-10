7c29e85f2554db24884e69b451b5217b637b5635
jung_wui <sonbal7762@gmail.com>
2026-09-10 18:10:35 +0900

    [Passed] 짝수만 2로 나누기 (2ms, 0MB)

--- trail2/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
+++ trail2/짝수만 2로 나누기/divide-even-numbers-by-2.cpp
@@ -5,16 +5,18 @@ using namespace std;
 int n;
 int arr[50];
 
-
+void Modify(int *arr) {
+    for(int i = 0; i < n ; i++){
+        if(arr[i]%2 ==0) arr[i] /= 2;
+    }
+}
 
 int main() {
     cin >> n;
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
-    for(int i = 0; i < n ; i++){
-        if(arr[i]%2 ==0) arr[i] /= 2;
-    }
+    Modify(arr);
     for (int i = 0; i < n; i++) {
         cout << arr[i] << " ";
     }

73aa11dd0154a11e520a406103f5170d257aaf72
jung_wui <sonbal7762@gmail.com>
2026-09-14 16:05:31 +0900

    [Passed] 홀수 짝수에 따른 출력값 (2ms, 0MB)

--- trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
+++ trail2/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.cpp
@@ -7,8 +7,7 @@ int N;
 int magicFunc(int n){
     if(n==1) return 1;
     if(n==0) return 0;
-    if(n % 2 == 0) return magicFunc(n-2) + n;
-    else return magicFunc(n-2) + n;
+    return magicFunc(n-2) + n;
 }
 
 int main() {

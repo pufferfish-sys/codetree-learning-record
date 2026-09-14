94d6e172fd6a5df3b199414d7b07fab56aea889b
jung_wui <sonbal7762@gmail.com>
2026-09-15 00:08:56 +0900

    [Passed] 중앙값 계산 2 (2ms, 0MB)

---
+++ trail2/중앙값 계산 2/get-median-2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int n;
+int arr[100];
+
+int main() {
+    cin >> n;
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    
+    for (int i = 0; i < n; i++){
+        if(i % 2 == 0) {
+        sort(arr,arr+i+1);    
+        cout << arr[i/2] << " ";}        
+    }
+
+    return 0;
+}

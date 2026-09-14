d5635b4d4f906edb4a752fc042e3891a0e4783c8
jung_wui <sonbal7762@gmail.com>
2026-09-14 17:05:15 +0900

    [Passed] 문자열 정렬 (2ms, 0MB)

---
+++ trail2/문자열 정렬/string-sort.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+string str;
+
+int main() {
+    cin >> str;
+    sort(str.begin(),str.end());
+    cout << str;
+    // Please write your code here.
+
+    return 0;
+}

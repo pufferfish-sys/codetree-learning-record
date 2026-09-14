0d6099753eabd5ac86d50344df3914f376f1d96c
jung_wui <sonbal7762@gmail.com>
2026-09-14 17:42:13 +0900

    [Passed] k번째로 신기한 문자열 (2ms, 0MB)

---
+++ trail2/k번째로 신기한 문자열/kth-special-string.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+int n, k;
+string t;
+string str[100], word[100];
+
+bool startwith(string a, string b){
+    for(int i = 0; i < (int) b.size(); i++){
+        if(a[i]!=b[i]) return false;
+    }
+    return true;
+}
+
+int main() {
+    cin >> n >> k >> t;
+    int cnt = 0;
+    for (int i = 0; i < n; i++) {
+        cin >> str[i];
+        if(startwith(str[i],t)) word[cnt++] = str[i];
+    }
+    sort(word, word + cnt);
+
+    cout << word[k-1];
+    
+
+    return 0;
+}

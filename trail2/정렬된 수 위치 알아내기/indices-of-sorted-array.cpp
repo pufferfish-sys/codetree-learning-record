98f887c22d17ec942ccf03d3aa2c9f957f667992
jung_wui <sonbal7762@gmail.com>
2026-09-15 23:00:19 +0900

    [Passed] 정렬된 수 위치 알아내기 (2ms, 0MB)

---
+++ trail2/정렬된 수 위치 알아내기/indices-of-sorted-array.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int N;
+int sequence[1000];
+
+class Info{
+    public:
+        int sequence;
+        int number;
+    Info(int sequence, int number)
+    : sequence(sequence), number(number){}
+
+    Info(){} 
+};
+
+Info info[1000];
+
+bool cmd(Info a, Info b){
+    if(a.sequence != b.sequence) return a.sequence < b.sequence;
+    return a.number < b.number;  
+}
+
+int main() {
+    cin >> N;
+    int answers[1000];
+    for (int i = 0; i < N; i++) {
+        cin >> sequence[i];
+        info[i] = Info(sequence[i],i+1);
+    }
+
+    sort(info,info+N,cmd);
+
+    for (int i = 0; i < N; i++) {
+        answers[info[i].number] = i + 1;
+    }
+    for (int i = 1; i <= N; i++) {
+        cout << answers[i] << " " ;
+    }
+
+    return 0;
+}
